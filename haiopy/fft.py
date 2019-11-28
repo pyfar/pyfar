74c96b9ed97c5609fc97c212bb243a5c72e293ef
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2019-11-28 18:17:16 +0100

    added documentation for the FFT and IFFT

--- haiopy/fft.py
+++ haiopy/fft.py
@@ -7,15 +7,58 @@ except ImportError:
 
 
 def rfftfreq(n_samples, sampling_rate):
+    """
+    Returns the positive discrete frequencies in the range `:math:[0, f_s/2]`
+    for which the FFT of a real valued time-signal with n_samples is
+    calculated. If the number of samples `:math:N` is even the number of
+    frequency bins will be `:math:2N+1`, if `:math:N` is odd, the number of
+    bins will be `:math:(N+1)/2`.
+
+    Parameters
+    ----------
+    n_samples : int
+        The number of samples in the signal
+    sampling_rate : int
+        The sampling rate of the signal
+
+    Returns
+    -------
+    frequencies : array, double
+        The positive discrete frequencies for which the FFT is calculated.
+    """
     return fft.rfftfreq(n_samples, d=1/sampling_rate)
 
 
 def rfft(data, n_samples, signal_type):
     """
+    Calculate the FFT of a real-valued time-signal. The function returns only
+    the right-hand side of the axis-symmetric spectrum. The normalization
+    distinguishes between energy and power signals. Energy signals are not
+    normalized in the forward transform. Power signals are normalized by their
+    number of samples and to their effective value as well as compensated for
+    the missing energy from the left-hand side of the spectrum. This ensures
+    that the energy of the time signal and the right-hand side of the spectrum
+    are equal and thus fulfill Parseval's theorem.
+
+    Parameters
+    ----------
+    data : array, double
+        Array containing the time domain signal with dimensions
+        (..., n_samples)
+    n_samples : int
+        The number of samples
+    signal_type : 'energy', 'power'
+        The signal type for normalization.
+
+    Returns
+    -------
+    spec : array, complex
+        The complex valued right-hand side of the spectrum with dimensions
+        (..., n_bins)
 
     """
-    sqrt_two = np.sqrt(2)
 
+    sqrt_two = np.sqrt(2)
     spec = fft.rfft(data, n=n_samples, axis=-1)
 
     if signal_type == 'energy':
@@ -33,6 +76,32 @@ def rfft(data, n_samples, signal_type):
 
 def irfft(spec, n_samples, signal_type):
     """
+    Calculate the IFFT of a axis-symmetric Fourier spectum. The function
+    takes only the right-hand side of the spectrum and returns a real-valued
+    time signal. The normalization distinguishes between energy and power
+    signals. Energy signals are not normalized by their number of samples.
+    Power signals are normalized by their effective value as well as
+    compensated for the missing energy from the left-hand side of the spectrum.
+    This ensures that the energy of the time signal and the right-hand side
+    of the spectrum are equal and thus fulfill Parseval's theorem.
+
+    Parameters
+    ----------
+    spec : array, complex
+        The complex valued right-hand side of the spectrum with dimensions
+        (..., n_bins)
+    n_samples : int
+        The number of samples in the corresponding tim signal. This is crucial
+        to allow for the correct transform of time signals with an odd number
+        of samples.
+    signal_type : 'energy', 'power'
+        The signal type for normalization.
+
+    Returns
+    -------
+    data : array, double
+        Array containing the time domain signal with dimensions
+        (..., n_samples)
     """
     sqrt_two = np.sqrt(2)
 
