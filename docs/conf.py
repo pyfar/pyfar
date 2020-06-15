872e331fe50ecf51cfbc9f0387069c3208afde8c
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-06-15 14:02:15 +0200

    sphinx extension: switched from numpydocs to napoleon

--- docs/conf.py
+++ docs/conf.py
@@ -35,7 +35,7 @@ import haiopy
 extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
-    'numpydoc',
+    'sphinx.ext.napoleon',
     'sphinx.ext.autosummary']
 
 # Add any paths that contain templates here, relative to this directory.
