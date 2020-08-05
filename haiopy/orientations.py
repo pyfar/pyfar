0ad57cca5f5a91d21cfbab87f3e10615251b91b2
Agent49 <kai.jurgeit@gmx.de>
2020-08-05 14:08:59 +0200

    Construct Orientations with view and up, tested with cart.

--- haiopy/orientations.py
+++ haiopy/orientations.py
@@ -22,29 +22,52 @@ class Orientations(object):
         - Roll/Pitch/Yaw angles
 
     """
-    def __init__(self, views=None, ups=None,
-                domain='cart', convention='right', unit=None,
-                weights=None, sh_order=None, comment=None):
+    def __init__(
+            self,
+            views=None,
+            ups=None,
+            domain='cart',
+            convention='right',
+            unit=None,
+            weights=None,
+            sh_order=None,
+            comment=None):
         if views is None:
             views = []
         if ups is None:
             ups = []
-        self._set_views_ups(views, ups,
-                domain, convention, unit,
-                weights, sh_order, comment)
+        self._set_views_ups(
+            views, ups, domain, convention, unit, weights, sh_order, comment)
 
-    def _set_views_ups(self, views, ups,
-                domain, convention, unit,
-                weights, sh_order, comment):
+    def _set_views_ups(
+            self,
+            views,
+            ups,
+            domain,
+            convention,
+            unit,
+            weights,
+            sh_order,
+            comment):
         """Check and set view and up vector(s)."""
         views = np.atleast_2d(views).astype(np.float64)
         ups = np.atleast_2d(ups).astype(np.float64)
 
+        # Validate
         if views.shape != ups.shape:
-            raise ValueError("There must be the same number of \
-                View and Up Vectors.")
+            raise ValueError(
+                "There must be the same number of View and Up Vectors.")
+        if views.size:
+            if views.shape[-1] not in (0, 3):
+                raise ValueError(
+                    "View and Up Vectors must be either empty or 3D.")
+            if not (
+                    np.all(np.linalg.norm(views, axis=1))
+                    and np.all(np.linalg.norm(ups, axis=1))):
+                raise ValueError(
+                    "If provided, View and Up Vectors must have a length")
 
-        # cast to Coordinates
+        # Cast to Coordinates
         try:
             views = Coordinates(
                 views[:, 0], views[:, 1], views[:, 2],
@@ -54,6 +77,11 @@ class Orientations(object):
                 ups[:, 0], ups[:, 1], ups[:, 2],
                 domain=domain, convention=convention, unit=unit,
                 weights=weights, sh_order=sh_order, comment=comment)
+            # Are View and Up vectors perpendicular?
+            if not np.allclose(
+                    0,
+                    np.einsum('ij,kj->k', views.get_cart(), ups.get_cart())):
+                raise ValueError("View and Up vectors are not perpendicular")
         except IndexError:
             views = Coordinates(
                 domain=domain, convention=convention, unit=unit,
@@ -61,5 +89,6 @@ class Orientations(object):
             ups = Coordinates(
                 domain=domain, convention=convention, unit=unit,
                 weights=weights, sh_order=sh_order, comment=comment)
-        
-        #
+
+        self.views = views
+        self.ups = ups
--- tests/test_orientations.py
+++ tests/test_orientations.py
@@ -1,8 +1,5 @@
-import numpy as np
-import numpy.testing as npt
 from pytest import raises
 
-import haiopy
 from haiopy import Orientations
 
 
@@ -22,10 +19,28 @@ def test_orientations_init_view_up():
     views = [[1, 0, 0], [0, 0, 1]]
     ups = [[0, 1, 0], [0, 1, 0]]
     Orientations(views, ups)
+    # mal-formed lists
+    views = [[1, 0, 0], [0, 0]]
+    ups = [[0, 1, 0], [0, 0, 0]]
+    with raises(ValueError):
+        Orientations(views, ups)
+    # any of views and ups has zero-length
+    views = [[1, 0, 0], [0, 0, 1]]
+    ups = [[0, 1, 0], [0, 0, 0]]
+    with raises(ValueError):
+        Orientations(views, ups)
     # view and up counts not matching
     views = [[1, 0, 0], [0, 0, 1]]
     ups = [[0, 1, 0]]
     with raises(ValueError):
         Orientations(views, ups)
-    # views and ups must be either empty or
-    
+    # views and ups must be either empty or 3D
+    views = [0, 1]
+    ups = [0, 1]
+    with raises(ValueError):
+        Orientations(views, ups)
+    # view and up vectors must be orthogonal
+    views = [1.0, 0.5, 0.1]
+    ups = [0, 0, 1]
+    with raises(ValueError):
+        Orientations(views, ups)
