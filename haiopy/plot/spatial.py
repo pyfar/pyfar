8b78f1dbfa2482038eecb7a0efb29f97c5ea30c0
Agent49 <kai.jurgeit@gmx.de>
2020-08-08 12:57:08 +0200

    Quiver works but yet contains redundandancies.

--- haiopy/plot/spatial.py
+++ haiopy/plot/spatial.py
@@ -78,3 +78,76 @@ def scatter(coordinates, projection='3d', ax=None, set_ax=True, **kwargs):
         ax.set_zlim(ax_lims)
 
     return ax
+
+
+def quiver(origins, endpoints, projection='3d', ax=None, set_ax=True, **kwargs):
+    """Plot vectors from their origins (x, y, z) to their endpoints (u, v, w).
+
+    Parameters
+    ----------
+    origins : Coordinates
+        The origins of the vectors.
+    endpoints : Coordinates
+        The endpoints of the vectors.
+    projection : '3d', 'ortho'
+        Projection to be used for the plot. Only three-dimensional projections
+        are supported.
+    ax : matplotlib.axis (optional)
+        If no axis is defined, a new axis in a new figure is created.
+    set_ax: boolean
+        Set the limits of the axis according to the points in coordinates. The
+        default is True.
+    **kwargs :
+        additional key value arguments are passed to matplotlib.pyplot.scatter.
+
+    Returns
+    ax : matplotlib.axes
+        The axis used for the plot.
+
+    """
+    if not (
+        isinstance(origins, Coordinates)
+        and isinstance(endpoints, Coordinates)):
+        raise ValueError("The coordinates need to be a Coordinates object")
+
+    if ax is None:
+        # create equal aspect figure for distortion free display
+        # (workaround for ax.set_aspect('equal', 'box'), which is currently not
+        #  working for 3D axes.)
+        plt.figure(figsize=plt.figaspect(1.))
+        ax = plt.gca(projection=projection)
+
+    if not 'Axes3D' in ax.__str__():
+        raise ValueError("Only three-dimensional axes supported.")
+
+    # add defaults to kwargs
+    if not 'marker' in kwargs:
+        kwargs['marker'] = '.'
+    if not 'c' in kwargs:
+        kwargs['c'] = 'k'
+
+    # copy to avoid changing the coordinate system of the original object
+    xyz = copy.deepcopy(origins).get_cart()
+    uvw = copy.deepcopy(endpoints).get_cart()
+
+    # plot
+    ax.quiver(*xyz.T, *uvw.T)
+
+    # labeling
+    ax.set_xlabel('X [m]')
+    ax.set_ylabel('Y [m]')
+    ax.set_zlabel('Z [m]')
+
+    # equal axis limits for distortion free  display
+    if set_ax:
+        # unfortunately ax.set_aspect('equal') does not work on Axes3D
+        min_val = min(np.min(xyz), np.min(uvw))
+        max_val = max(np.max(xyz), np.max(uvw))
+        ax_lims = (min_val-.15*np.abs(min_val),
+                   max_val+.15*np.abs(max_val))
+
+        ax.set_xlim(ax_lims)
+        ax.set_ylim(ax_lims)
+        ax.set_zlim(ax_lims)
+
+    return ax
