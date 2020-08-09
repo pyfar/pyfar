26f3cbe19dd21afc72fbb072644bcedd06bdaf52
Agent49 <kai.jurgeit@gmx.de>
2020-08-09 19:18:50 +0200

    DRY-refactoring in haiopy.plot.spatial.

--- haiopy/plot/spatial.py
+++ haiopy/plot/spatial.py
@@ -26,7 +26,7 @@ def scatter(coordinates, projection='3d', ax=None, set_ax=True, **kwargs):
         Set the limits of the axis according to the points in coordinates. The
         default is True.
     **kwargs :
-        additional key value arguments are passed to matplotlib.pyplot.scatter.
+        Additional key value arguments are passed to matplotlib.pyplot.scatter.
 
     Returns
     ax : matplotlib.axes
@@ -36,25 +36,11 @@ def scatter(coordinates, projection='3d', ax=None, set_ax=True, **kwargs):
     if not isinstance(coordinates, Coordinates):
         raise ValueError("The coordinates need to be a Coordinates object")
 
-    if ax is None:
-        # create equal aspect figure for distortion free display
-        # (workaround for ax.set_aspect('equal', 'box'), which is currently not
-        #  working for 3D axes.)
-        plt.figure(figsize=plt.figaspect(1.))
-        ax = plt.gca(projection=projection)
-
-    if not 'Axes3D' in ax.__str__():
-        raise ValueError("Only three-dimensional axes supported.")
-
-    # add defaults to kwargs
-    if not 'marker' in kwargs:
-        kwargs['marker'] = '.'
-    if not 'c' in kwargs:
-        kwargs['c'] = 'k'
-
     # copy to avoid changing the coordinate system of the original object
-    c   = copy.deepcopy(coordinates)
-    xyz = c.get_cart()
+    xyz = copy.deepcopy(coordinates).get_cart()
+
+    ax = _setup_axes(
+        projection, ax, set_ax, bounds=(np.min(xyz), np.max(xyz)), **kwargs)
 
     # plot
     ax.scatter(
@@ -67,20 +53,11 @@ def scatter(coordinates, projection='3d', ax=None, set_ax=True, **kwargs):
     ax.set_ylabel('Y [m]')
     ax.set_zlabel('Z [m]')
 
-    # equal axis limits for distortion free  display
-    if set_ax:
-        # unfortunately ax.set_aspect('equal') does not work on Axes3D
-        ax_lims = (np.min(xyz)-.15*np.abs(np.min(xyz)),
-                   np.max(xyz)+.15*np.abs(np.max(xyz)))
-
-        ax.set_xlim(ax_lims)
-        ax.set_ylim(ax_lims)
-        ax.set_zlim(ax_lims)
-
     return ax
 
 
-def quiver(origins, endpoints, projection='3d', ax=None, set_ax=True, **kwargs):
+def quiver(
+        origins, endpoints, projection='3d', ax=None, set_ax=True, **kwargs):
     """Plot vectors from their origins (x, y, z) to their endpoints (u, v, w).
 
     Parameters
@@ -98,7 +75,7 @@ def quiver(origins, endpoints, projection='3d', ax=None, set_ax=True, **kwargs):
         Set the limits of the axis according to the points in coordinates. The
         default is True.
     **kwargs :
-        additional key value arguments are passed to matplotlib.pyplot.scatter.
+        Additional key value arguments are passed to matplotlib.pyplot.scatter.
 
     Returns
     ax : matplotlib.axes
@@ -106,10 +83,51 @@ def quiver(origins, endpoints, projection='3d', ax=None, set_ax=True, **kwargs):
 
     """
     if not (
-        isinstance(origins, Coordinates)
-        and isinstance(endpoints, Coordinates)):
-        raise ValueError("The coordinates need to be a Coordinates object")
+            isinstance(origins, Coordinates)
+            and isinstance(endpoints, Coordinates)):
+        raise ValueError(
+            "The origins and endpoints need to be Coordinates objects")
 
+    # copy to avoid changing the coordinate system of the original object
+    xyz = copy.deepcopy(origins).get_cart()
+    uvw = copy.deepcopy(endpoints).get_cart()
+
+    min_val = min(np.min(xyz), np.min(uvw))
+    max_val = max(np.max(xyz), np.max(uvw))
+    ax = _setup_axes(
+        projection, ax, set_ax, bounds=(min_val, max_val), **kwargs)
+
+    # plot
+    ax.quiver(*xyz.T, *uvw.T)
+
+    return ax
+
+
+def _setup_axes(
+        projection='3d', ax=None, set_ax=True, bounds=(-1, 1), **kwargs):
+    """Setup axes' limits and labels for 3D-plots.
+
+    Parameters
+    ----------
+    projection : '3d', 'ortho'
+        Projection to be used for the plot. Only three-dimensional projections
+        are supported.
+    ax : matplotlib.axis (optional)
+        If no axis is defined, a new axis in a new figure is created.
+    set_ax: boolean
+        Set the limits of the axis according to the points in coordinates. The
+        default is True.
+    bounds: tuple (min, max)
+        The lower and upper boundaries of the data to be plotted. This is used
+        for the axes' limits.
+    **kwargs :
+        Additional key value arguments are passed to matplotlib.pyplot.scatter.
+
+    Returns
+    ax : matplotlib.axes
+        The axis used for the plot.
+
+    """
     if ax is None:
         # create equal aspect figure for distortion free display
         # (workaround for ax.set_aspect('equal', 'box'), which is currently not
@@ -117,21 +135,14 @@ def quiver(origins, endpoints, projection='3d', ax=None, set_ax=True, **kwargs):
         plt.figure(figsize=plt.figaspect(1.))
         ax = plt.gca(projection=projection)
 
-    if not 'Axes3D' in ax.__str__():
+    if 'Axes3D' not in ax.__str__():
         raise ValueError("Only three-dimensional axes supported.")
 
     # add defaults to kwargs
-    if not 'marker' in kwargs:
+    if 'marker' not in kwargs:
         kwargs['marker'] = '.'
-    if not 'c' in kwargs:
+    if 'c' not in kwargs:
         kwargs['c'] = 'k'
-
-    # copy to avoid changing the coordinate system of the original object
-    xyz = copy.deepcopy(origins).get_cart()
-    uvw = copy.deepcopy(endpoints).get_cart()
-
-    # plot
-    ax.quiver(*xyz.T, *uvw.T)
 
     # labeling
     ax.set_xlabel('X [m]')
@@ -141,10 +152,8 @@ def quiver(origins, endpoints, projection='3d', ax=None, set_ax=True, **kwargs):
     # equal axis limits for distortion free  display
     if set_ax:
         # unfortunately ax.set_aspect('equal') does not work on Axes3D
-        min_val = min(np.min(xyz), np.min(uvw))
-        max_val = max(np.max(xyz), np.max(uvw))
-        ax_lims = (min_val-.15*np.abs(min_val),
-                   max_val+.15*np.abs(max_val))
+        ax_lims = (bounds[0]-.15*np.abs(bounds[0]),
+                   bounds[1]+.15*np.abs(bounds[1]))
 
         ax.set_xlim(ax_lims)
         ax.set_ylim(ax_lims)
