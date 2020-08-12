c3ae8aa06d9d66c5f661dba1fa05c3a5ff04a58c
Agent49 <kai.jurgeit@gmx.de>
2020-08-12 21:19:00 +0200

    Orientations.from_view_up works with list, ndarray and Coordinates() AND tests implemented.

--- haiopy/orientations.py
+++ haiopy/orientations.py
@@ -8,13 +8,44 @@ from scipy.spatial.transform import Rotation
 
 
 class Orientations(Rotation):
-    
-    
+
+
     def __init__(self, quat=None, normalize=True, copy=True):
         if quat is None:
             quat = np.array([0., 0., 0., 1.])
         super().__init__(quat, normalize=normalize, copy=copy)
+
+    @classmethod
+    def from_view_up(cls, views, ups):
+        try:
+            views = views.get_cart()
+            ups = ups.get_cart()
+        except AttributeError:
+            views = np.atleast_2d(views).astype(np.float64)
+            ups = np.atleast_2d(ups).astype(np.float64)
+        if views.shape != ups.shape:
+            raise ValueError(
+                "There must be the same number of View and Up Vectors.")
+        if views.shape[-1] != 3:
+            raise ValueError(f"Expected `views` and `ups` to have shape (N, 3)"
+                             " or (3,), got {views.shape}")
+
+        if not (np.all(np.linalg.norm(views, axis=1))
+                and np.all(np.linalg.norm(ups, axis=1))):
+            raise ValueError("View and Up Vectors must have a length.")
+        if not np.allclose(0, np.einsum('ij,kj->k', views, ups)):
+            raise ValueError("View and Up vectors must be perpendicular.")
         
+        rights = np.cross(views, ups)
+        
+        rotation_matrix = np.empty((views.shape[0], views.shape[1], 3))
+        
+        rotation_matrix[:, 0, :3] = views
+        rotation_matrix[:, 1, :3] = ups
+        rotation_matrix[:, 2, :3] = rights
+
+        return cls.from_matrix(rotation_matrix)
+
 
     def show(self, positions=None):
         """
@@ -22,7 +53,7 @@ class Orientations(Rotation):
 
         Parameters
         ----------
-        
+
         mask : boolean numpy array, None
             Plot points in black if mask==True and red if mask==False. The
             default is None, in which case all points are plotted in black.
@@ -40,7 +71,7 @@ class Orientations(Rotation):
         elif positions.cshape[0] != self._quat.shape[0]:
             raise ValueError("If provided, there must be the same number"
                              "of positions as orientations.")
-            
+
         # Create view, up and right vectors from Rotation object
         views, rights, ups = [np.atleast_2d(self.apply(x)) for x in np.eye(3)]
         views, rights, ups = [Coordinates(x[:, 0], x[:, 1], x[:, 2])
--- tests/test_orientations.py
+++ tests/test_orientations.py
@@ -1,5 +1,7 @@
 from pytest import raises
 
+import numpy as np
+
 from haiopy.orientations import Orientations
 from haiopy.coordinates import Coordinates
 
@@ -9,23 +11,73 @@ def test_orientations_init():
     orient = Orientations()
     assert isinstance(orient, Orientations)
     
+    
+def test_orientations_from_view_up():
+    # test with single view and up vectors
+    view = [1, 0, 0]
+    up = [0, 1, 0]
+    Orientations.from_view_up(view, up)
+    # test with multiple view and up vectors
+    views = [[1, 0, 0], [0, 0, 1]]
+    ups = [[0, 1, 0], [0, 1, 0]]
+    Orientations.from_view_up(views, ups)
+    # provided as ndarrays
+    views = np.atleast_2d(views).astype(np.float64)
+    ups = np.atleast_2d(ups).astype(np.float64)
+    Orientations.from_view_up(views, ups)
+    # provided as Coordinates
+    views = Coordinates(views[:, 0], views[:, 1], views[:, 2])
+    ups = Coordinates(ups[:, 0], ups[:, 1], ups[:, 2])
+    Orientations.from_view_up(views, ups)
+    
+
+def test_orientations_from_view_up_invalid():
+    # mal-formed lists
+    views = [[1, 0, 0], [0, 0]]
+    ups = [[0, 1, 0], [0, 0, 0]]
+    with raises(ValueError):
+        Orientations.from_view_up(views, ups)
+    # any of views and ups has zero-length
+    views = [[1, 0, 0], [0, 0, 1]]
+    ups = [[0, 1, 0], [0, 0, 0]]
+    with raises(ValueError):
+        Orientations.from_view_up(views, ups)
+    # view and up counts not matching
+    views = [[1, 0, 0], [0, 0, 1]]
+    ups = [[0, 1, 0]]
+    with raises(ValueError):
+        Orientations.from_view_up(views, ups)
+    # views' and ups' shape must be (N, 3) or (3,)
+    views = [0, 1]
+    ups = [0, 1]
+    with raises(ValueError):
+        Orientations.from_view_up(views, ups)
+    # view and up vectors must be orthogonal
+    views = [1.0, 0.5, 0.1]
+    ups = [0, 0, 1]
+    with raises(ValueError):
+        Orientations.from_view_up(views, ups)
+
 
 def test_orientations_show():
     """Test method show with and without positions"""
-    orientations = Orientations()
-    # TODO: raise specific exception
+    # single vectors no position
+    view = [1, 0, 0]
+    up = [0, 1, 0]
+    orientation = Orientations.from_view_up(view, up)
+    orientation.show()
+    # with position
+    position = Coordinates(0, 1, 0)
+    orientation.show(position)
+    # multiple vectors no position
     views = [[1, 0, 0], [0, 0, 1]]
     ups = [[0, 1, 0], [0, 1, 0]]
-    orientations = Orientations(views, ups)
-    # without positions
+    orientations = Orientations.from_view_up(views, ups)
     orientations.show()
-    # wrong type
-    with raises(TypeError):
-        orientations.show([0, 1, 0])
-    # with positions of differing size
+    # with matching number of positions
+    positions = Coordinates([0, 5], [0, 0], [0, 0])
+    orientations.show(positions)
+    # with non-matching positions
     positions = Coordinates(0, 1, 0)
     with raises(ValueError):
         orientations.show(positions)
-    positions = Coordinates([0, 5], [0, 0], [0, 0])
-    orientations.show(positions)
-    pass
