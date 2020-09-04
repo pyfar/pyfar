9034fb9c4e5af09c6758d79905b75bb3498a8c3f
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-09-04 19:49:41 +0200

    test fft functions with the numpy and pyfftw implementations

--- tests/test_fft.py
+++ tests/test_fft.py
@@ -73,7 +73,7 @@ def test_fft_orthogonality_noise_odd_lib():
     npt.assert_allclose(signal_time, transformed_signal_time, rtol=1e-10)
 
 
-def test_fft_orthogonality_sine_even():
+def test_fft_orthogonality_sine_even_np(fft_lib_np):
     num_samples = 2**10
     frequency = 10e3
     samplingrate = 40e3
@@ -90,7 +90,24 @@ def test_fft_orthogonality_sine_even():
     npt.assert_allclose(signal_time, transformed_signal_time, rtol=1e-10)
 
 
-def test_fft_orthogonality_sine_odd():
+def test_fft_orthogonality_sine_even_fftw(fft_lib_pyfftw):
+    num_samples = 2**10
+    frequency = 10e3
+    samplingrate = 40e3
+    num_periods = np.floor(num_samples / samplingrate * frequency)
+    # round to the nearest frequency resulting in a fully periodic sine signal
+    # in the given time interval
+    frequency = num_periods * samplingrate / num_samples
+    times = np.arange(0, num_samples) / samplingrate
+
+    signal_time = 1 * np.cos(2 * np.pi * frequency * times)
+    signal_spec = fft.rfft(signal_time, num_samples, 'power')
+    transformed_signal_time = fft.irfft(signal_spec, num_samples, 'power')
+
+    npt.assert_allclose(signal_time, transformed_signal_time, rtol=1e-10)
+
+
+def test_fft_orthogonality_sine_odd_np(fft_lib_np):
     num_samples = 2**10+3
     frequency = 10e3
     samplingrate = 40e3
@@ -107,7 +124,24 @@ def test_fft_orthogonality_sine_odd():
     npt.assert_allclose(signal_time, transformed_signal_time, rtol=1e-10)
 
 
-def test_fft_orthogonality_noise_even():
+def test_fft_orthogonality_sine_odd_fftw(fft_lib_pyfftw):
+    num_samples = 2**10+3
+    frequency = 10e3
+    samplingrate = 40e3
+    num_periods = np.floor(num_samples / samplingrate * frequency)
+    # round to the nearest frequency resulting in a fully periodic sine signal
+    # in the given time interval
+    frequency = num_periods * samplingrate / num_samples
+    times = np.arange(0, num_samples) / samplingrate
+
+    signal_time = 1 * np.cos(2 * np.pi * frequency * times)
+    signal_spec = fft.rfft(signal_time, num_samples, 'power')
+    transformed_signal_time = fft.irfft(signal_spec, num_samples, 'power')
+
+    npt.assert_allclose(signal_time, transformed_signal_time, rtol=1e-10)
+
+
+def test_fft_orthogonality_noise_even_np(fft_lib_np):
     n_samples = 2**18
     np.random.seed(450)
     signal_time = np.random.normal(0, 1, n_samples)
@@ -117,7 +151,17 @@ def test_fft_orthogonality_noise_even():
     npt.assert_allclose(signal_time, transformed_signal_time, rtol=1e-8)
 
 
-def test_fft_orthogonality_noise_odd():
+def test_fft_orthogonality_noise_even_fftw(fft_lib_pyfftw):
+    n_samples = 2**18
+    np.random.seed(450)
+    signal_time = np.random.normal(0, 1, n_samples)
+    signal_spec = fft.rfft(signal_time, n_samples, 'power')
+    transformed_signal_time = fft.irfft(signal_spec, n_samples, 'power')
+
+    npt.assert_allclose(signal_time, transformed_signal_time, rtol=1e-8)
+
+
+def test_fft_orthogonality_noise_odd_np(fft_lib_np):
     n_samples = 2**18+1
     np.random.seed(450)
     signal_time = np.random.normal(0, 1, n_samples)
@@ -127,7 +171,17 @@ def test_fft_orthogonality_noise_odd():
     npt.assert_allclose(signal_time, transformed_signal_time, rtol=1e-8)
 
 
-def test_fft_parsevaL_theorem_sine_even():
+def test_fft_orthogonality_noise_odd_fftw(fft_lib_pyfftw):
+    n_samples = 2**18+1
+    np.random.seed(450)
+    signal_time = np.random.normal(0, 1, n_samples)
+    signal_spec = fft.rfft(signal_time, n_samples, 'power')
+    transformed_signal_time = fft.irfft(signal_spec, n_samples, 'power')
+
+    npt.assert_allclose(signal_time, transformed_signal_time, rtol=1e-8)
+
+
+def test_fft_parsevaL_theorem_sine_even_np(fft_lib_np):
     num_samples = 2**10
     frequency = 10e3
     samplingrate = 40e3
@@ -146,7 +200,26 @@ def test_fft_parsevaL_theorem_sine_even():
     npt.assert_allclose(e_time, e_freq, rtol=1e-10)
 
 
-def test_fft_parsevaL_theorem_sine_odd():
+def test_fft_parsevaL_theorem_sine_even_fftw(fft_lib_pyfftw):
+    num_samples = 2**10
+    frequency = 10e3
+    samplingrate = 40e3
+    num_periods = np.floor(num_samples / samplingrate * frequency)
+    # round to the nearest frequency resulting in a fully periodic sine signal
+    # in the given time interval
+    frequency = num_periods * samplingrate / num_samples
+    times = np.arange(0, num_samples) / samplingrate
+
+    signal_time = 1 * np.cos(2 * np.pi * frequency * times)
+    signal_spec = fft.rfft(signal_time, num_samples, 'power')
+
+    e_time = np.mean(np.abs(signal_time)**2)
+    e_freq = np.sum(np.abs(signal_spec)**2)
+
+    npt.assert_allclose(e_time, e_freq, rtol=1e-10)
+
+
+def test_fft_parsevaL_theorem_sine_odd_np(fft_lib_np):
     num_samples = 2**10+3
     frequency = 10e3
     samplingrate = 40e3
@@ -165,10 +238,29 @@ def test_fft_parsevaL_theorem_sine_odd():
     npt.assert_allclose(e_time, e_freq, rtol=1e-10)
 
 
-def test_fft_parsevaL_theorem_noise_even():
+def test_fft_parsevaL_theorem_sine_odd_fftw(fft_lib_pyfftw):
+    num_samples = 2**10+3
+    frequency = 10e3
+    samplingrate = 40e3
+    num_periods = np.floor(num_samples / samplingrate * frequency)
+    # round to the nearest frequency resulting in a fully periodic sine signal
+    # in the given time interval
+    frequency = num_periods * samplingrate / num_samples
+    times = np.arange(0, num_samples) / samplingrate
+
+    signal_time = 1 * np.cos(2 * np.pi * frequency * times)
+    signal_spec = fft.rfft(signal_time, num_samples, 'power')
+
+    e_time = np.mean(np.abs(signal_time)**2)
+    e_freq = np.sum(np.abs(signal_spec)**2)
+
+    npt.assert_allclose(e_time, e_freq, rtol=1e-10)
+
+
+def test_fft_parsevaL_theorem_noise_even_np(fft_lib_np):
     n_samples = 2**20
     np.random.seed(450)
-    noise_time = np.random.normal(0,1,n_samples)
+    noise_time = np.random.normal(0, 1, n_samples)
     noise_spec = fft.rfft(noise_time, n_samples, 'power')
 
     e_time = np.mean(np.abs(noise_time)**2)
@@ -177,10 +269,34 @@ def test_fft_parsevaL_theorem_noise_even():
     npt.assert_allclose(e_time, e_freq, rtol=1e-10)
 
 
-def test_fft_parsevaL_theorem_noise_odd():
+def test_fft_parsevaL_theorem_noise_even_fftw(fft_lib_pyfftw):
+    n_samples = 2**20
+    np.random.seed(450)
+    noise_time = np.random.normal(0, 1, n_samples)
+    noise_spec = fft.rfft(noise_time, n_samples, 'power')
+
+    e_time = np.mean(np.abs(noise_time)**2)
+    e_freq = np.sum(np.abs(noise_spec)**2)
+
+    npt.assert_allclose(e_time, e_freq, rtol=1e-10)
+
+
+def test_fft_parsevaL_theorem_noise_odd_np(fft_lib_np):
     n_samples = 2**20+1
     np.random.seed(450)
-    noise_time = np.random.normal(0,1,n_samples)
+    noise_time = np.random.normal(0, 1, n_samples)
+    noise_spec = fft.rfft(noise_time, n_samples, 'power')
+
+    e_time = np.mean(np.abs(noise_time)**2)
+    e_freq = np.sum(np.abs(noise_spec)**2)
+
+    npt.assert_allclose(e_time, e_freq, rtol=1e-10)
+
+
+def test_fft_parsevaL_theorem_noise_odd_fftw(fft_lib_pyfftw):
+    n_samples = 2**20+1
+    np.random.seed(450)
+    noise_time = np.random.normal(0, 1, n_samples)
     noise_spec = fft.rfft(noise_time, n_samples, 'power')
 
     e_time = np.mean(np.abs(noise_time)**2)
@@ -199,7 +315,7 @@ def test_is_not_odd():
     assert not fft._is_odd(num)
 
 
-def test_rfft_energy_imp_even_samples(impulse):
+def test_rfft_energy_imp_even_samples_np(impulse, fft_lib_np):
     n_samples = 1024
     spec = fft.rfft(impulse, n_samples, 'energy')
 
@@ -207,7 +323,15 @@ def test_rfft_energy_imp_even_samples(impulse):
     npt.assert_allclose(spec, truth)
 
 
-def test_irfft_energy_imp_even_samples(impulse):
+def test_rfft_energy_imp_even_samples_fftw(impulse, fft_lib_pyfftw):
+    n_samples = 1024
+    spec = fft.rfft(impulse, n_samples, 'energy')
+
+    truth = np.ones(int(n_samples/2+1), dtype=np.complex)
+    npt.assert_allclose(spec, truth)
+
+
+def test_irfft_energy_imp_even_samples_np(impulse, fft_lib_np):
     n_samples = 1024
     spec = np.ones(int(n_samples/2+1), dtype=np.complex)
     data = fft.irfft(spec, n_samples, 'energy')
@@ -216,9 +340,17 @@ def test_irfft_energy_imp_even_samples(impulse):
     npt.assert_allclose(data, truth)
 
 
-def test_rfft_power_imp_even_samples(sine):
+def test_irfft_energy_imp_even_samples_np_fftw(impulse, fft_lib_pyfftw):
     n_samples = 1024
-    sampling_rate = 2e3
+    spec = np.ones(int(n_samples/2+1), dtype=np.complex)
+    data = fft.irfft(spec, n_samples, 'energy')
+
+    truth = impulse
+    npt.assert_allclose(data, truth)
+
+
+def test_rfft_power_imp_even_samples_np(sine, fft_lib_np):
+    n_samples = 1024
     spec = fft.rfft(sine, n_samples, 'power')
 
     truth = np.zeros(int(n_samples/2+1), dtype=np.complex)
@@ -227,7 +359,17 @@ def test_rfft_power_imp_even_samples(sine):
     npt.assert_allclose(np.imag(spec), np.imag(truth), atol=1e-10)
 
 
-def test_irfft_power_imp_even_samples(sine):
+def test_rfft_power_imp_even_samples_fftw(sine, fft_lib_pyfftw):
+    n_samples = 1024
+    spec = fft.rfft(sine, n_samples, 'power')
+
+    truth = np.zeros(int(n_samples/2+1), dtype=np.complex)
+    truth[int(n_samples/16)] = 1/np.sqrt(2)
+    npt.assert_allclose(np.real(spec), np.real(truth), atol=1e-10)
+    npt.assert_allclose(np.imag(spec), np.imag(truth), atol=1e-10)
+
+
+def test_irfft_power_imp_even_samples_np(sine, fft_lib_np):
     n_samples = 1024
     spec = np.zeros(int(n_samples/2+1), dtype=np.complex)
     spec[int(n_samples/16)] = 1/np.sqrt(2)
@@ -238,7 +380,18 @@ def test_irfft_power_imp_even_samples(sine):
     npt.assert_allclose(data, truth, atol=1e-10)
 
 
-def test_irfft_power_imp_odd_samples(sine_odd):
+def test_irfft_power_imp_even_samples_fftw(sine, fft_lib_pyfftw):
+    n_samples = 1024
+    spec = np.zeros(int(n_samples/2+1), dtype=np.complex)
+    spec[int(n_samples/16)] = 1/np.sqrt(2)
+
+    data = fft.irfft(spec, n_samples, 'power')
+
+    truth = sine
+    npt.assert_allclose(data, truth, atol=1e-10)
+
+
+def test_irfft_power_imp_odd_samples_np(sine_odd, fft_lib_np):
     n_samples = 1023
     spec = np.zeros(int((n_samples+1)/2), dtype=np.complex)
     spec[int(n_samples/16)] = 1/np.sqrt(2)
@@ -249,9 +402,30 @@ def test_irfft_power_imp_odd_samples(sine_odd):
     npt.assert_allclose(data, truth, atol=1e-10)
 
 
-def test_rfft_power_imp_odd_samples(sine_odd):
+def test_irfft_power_imp_odd_samples_fftw(sine_odd, fft_lib_pyfftw):
     n_samples = 1023
-    sampling_rate = 2e3
+    spec = np.zeros(int((n_samples+1)/2), dtype=np.complex)
+    spec[int(n_samples/16)] = 1/np.sqrt(2)
+
+    data = fft.irfft(spec, n_samples, 'power')
+
+    truth, f = sine_odd
+    npt.assert_allclose(data, truth, atol=1e-10)
+
+
+def test_rfft_power_imp_odd_samples_np(sine_odd, fft_lib_np):
+    n_samples = 1023
+    s, f = sine_odd
+    spec = fft.rfft(s, n_samples, 'power')
+
+    truth = np.zeros(int((n_samples+1)/2), dtype=np.complex)
+    truth[int(n_samples/16)] = 1/np.sqrt(2)
+    npt.assert_allclose(np.real(spec), np.real(truth), atol=1e-10)
+    npt.assert_allclose(np.imag(spec), np.imag(truth), atol=1e-10)
+
+
+def test_rfft_power_imp_odd_samples_fftw(sine_odd, fft_lib_pyfftw):
+    n_samples = 1023
     s, f = sine_odd
     spec = fft.rfft(s, n_samples, 'power')
 
@@ -357,8 +531,8 @@ def sine_odd():
 
     if fullperiod:
         num_periods = np.floor(num_samples / samplingrate * frequency)
-        # round to the nearest frequency resulting in a fully periodic sine signal
-        # in the given time interval
+        # round to the nearest frequency resulting in a fully periodic sine
+        # signal in the given time interval
         frequency = num_periods * samplingrate / num_samples
     times = np.arange(0, num_samples) / samplingrate
     signal = amplitude * np.cos(2 * np.pi * frequency * times)
