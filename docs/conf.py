bd4d2767ca4ebd1fcf35b863e02417e2bf03b73b
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-09-04 22:03:03 +0200

    add sphinx matplotlib extions for plots in docstrings

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,8 @@ extensions = [
     'sphinx.ext.autodoc',
     'sphinx.ext.viewcode',
     'sphinx.ext.napoleon',
-    'sphinx.ext.autosummary']
+    'sphinx.ext.autosummary',
+    'matplotlib.sphinxext.plot_directive']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
