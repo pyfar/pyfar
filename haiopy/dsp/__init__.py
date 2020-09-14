87621ec587c69ba04f4ecb117193f9c532b2baa8
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-09-14 09:35:10 +0200

    add missing init

---
+++ haiopy/dsp/__init__.py
@@ -0,0 +1,9 @@
+from .filter import Filter
+from .filter import FilterFIR
+from .filter import FilterIIR
+from .filter import FilterSOS
+
+
+__all__ = [
+    Filter, FilterFIR, FilterIIR, FilterSOS
+]
