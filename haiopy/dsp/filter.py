7150b8a866b966d17960110ee0b643400765b52a
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-09-24 07:58:51 +0200

    wip: working on unified interface for filter class

--- haiopy/dsp/filter.py
+++ haiopy/dsp/filter.py
@@ -1,23 +1,69 @@
 import copy
+import warnings
 
 import numpy as np
 import scipy.signal as spsignal
+
+from haiopy import Signal
+
+
+def atleast_3d_first_dim(arr):
+    arr = np.asarray(arr)
+    ndim = np.ndim(arr)
+
+    if ndim < 2:
+        arr = np.atleast_2d(arr)
+    if ndim < 3:
+        return arr[np.newaxis]
+    else:
+        return arr
+
+
+def lfilter(coefficients, signal, zi):
+    return spsignal.lfilter(coefficients[0], coefficients[1], signal, zi=zi)
+
+
+def filtfilt(coefficients, signal, **kwargs):
+    kwargs.pop('zi', None)
+    return spsignal.filtfilt(
+        coefficients[0], coefficients[1], signal, **kwargs)
+
+
+def sosfilt(sos, signal, zi):
+    return spsignal.sosfilt(sos, signal, zi=zi)
+
+
+def sosfiltfilt(sos, signal, **kwargs):
+    kwargs.pop('zi', None)
+    return spsignal.sosfiltfilt(sos, signal, **kwargs)
 
 
 class Filter(object):
     def __init__(
             self,
-            coefficients,
-            filter_func=spsignal.lfilter,
+            coefficients=None,
+            filter_func=None,
             state=None):
         super().__init__()
+        if coefficients is not None:
+            coefficients = atleast_3d_first_dim(coefficients)
         self._coefficients = coefficients
-        self._filter_func = filter_func
-        if state is None:
-            zi = spsignal.lfilter_zi(coefficients[0], coefficients[1])
-            self._state = np.zeros_like(zi)
+        if state is not None:
+            if coefficients is None:
+                raise ValueError("Cannot set a state without filter coefficients")
+            state = atleast_3d_first_dim(state)
+            self._initialized = True
         else:
-            self._state = state
+            self._initialized = False
+        self._state = state
+        self._filter_func = None
+
+        self._FILTER_FUNCS = {
+            'default': None,
+            'minimumphase': None}
+
+    def initialize(self):
+        raise NotImplementedError("Abstract class method")
 
     @property
     def state(self):
@@ -25,34 +71,80 @@ class Filter(object):
 
     @property
     def filter_func(self):
+        raise NotImplementedError("Abstract class method")
+
+    @filter_func.setter
+    def filter_func(self, filter_func):
+        raise NotImplementedError("Abstract class method")
+
+    def process(self, signal, reset=True):
+        """Apply the filter to a signal.
+        """
+        # if not isinstance(signal, Signal):
+        #     raise ValueError("The input needs to be a haiopy.Signal object.")
+
+        for coeff, state in zip(self._coefficients, self._state):
+            filtered, new_state = self.filter_func(
+                coeff, signal.time, state)
+
+        # filtered_signal = copy.deepcopy(signal)
+        # filtered_signal.time = filtered
+        return filtered
+
+    def reset(self):
+        if self._state is not None:
+            self._state = np.zeros_like(self._state)
+
+
+class FilterIIR(Filter):
+    def __init__(
+            self,
+            coefficients,
+            filter_func=lfilter):
+        """IIR filter
+        """
+        coeff = np.atleast_2d(coefficients)
+        super().__init__(coefficients=coeff)
+
+        self._FILTER_FUNCS = {
+            'default': lfilter,
+            'minimumphase': filtfilt}
+        self._filter_func = filter_func
+
+    @property
+    def filter_func(self):
         return self._filter_func
 
     @filter_func.setter
     def filter_func(self, filter_func):
+        if type('filter_func') == str:
+            filter_func = self._FILTER_FUNCS[filter_func]
         self._filter_func = filter_func
-
-    def process(self, signal):
-        filtered, state = self.filter_func(signal.time, self.state)
-        self.state = state
-        signal.time = filtered
-        filtered_signal = copy.deepcopy(signal)
-        filtered_signal.time = filtered
-        return filtered_signal
-
-    def reset(self):
-        self._state = np.zeros_like(self._state)
-
-
-class FilterIIR(Filter):
-    def __init__(self, coefficients, form='sos'):
-        super().__init__()
-
-
-class FilterFIR(Filter):
-    def __init__(self):
-        super().__init__()
 
 
 class FilterSOS(Filter):
-    def __init__(self):
+    def __init__(
+            self,
+            coefficients,
+            filter_func=sosfilt):
+        coeff = np.atleast_2d(coefficients)
+        if coeff.shape[-1] != 6:
+            raise ValueError("The coefficients are not in line with a second order section filter structure.")
+        super().__init__(
+            coefficients=coeff)
+
+        self._FILTER_FUNCS = {
+            'default': sosfilt,
+            'minimumphase': sosfiltfilt
+        }
+        self._filter_func = filter_func
+
+    @property
+    def filter_func(self):
+        return self._filter_func
+
+
+class FilterFIR(Filter):
+    def __init__(self, coefficients):
+        coeff = np.atleast_2d(coefficients)
         super().__init__()
--- tests/test_filter.py
+++ tests/test_filter.py
@@ -1,30 +1,103 @@
+import pytest
 import numpy as np
 import numpy.testing as npt
-from haiopy import dsp
+import haiopy.dsp.filter as hpfilter
+from haiopy import Signal
+
+import matplotlib.pyplot as plt
+
+
+def test_filter_init_empty_coefficients():
+    filt = hpfilter.Filter(coefficients=None, state=None)
+    assert filt._coefficients is None
+    assert filt._state is None
+
+
+def test_filter_init_empty_coefficients_with_state():
+    with pytest.raises(ValueError):
+        hpfilter.Filter(coefficients=None, state=[1, 0])
 
 
 def test_filter_init():
-    coeff = [[1, 0, 0], [1, 0, 0]]
-    filt = dsp.Filter(coeff)
+    coeff = np.array([[[1, 0, 0], [1, 0, 0]]])
+    filt = hpfilter.Filter(coefficients=coeff)
     npt.assert_array_equal(filt._coefficients, coeff)
 
 
-def test_filter_init_state_empty():
-    coeff = [[1, 0, 0], [1, 0, 0]]
-    filt = dsp.Filter(coeff)
-    npt.assert_array_equal(filt._state, np.zeros(2))
+def test_filter_init_empty_state():
+    coeff = np.array([[[1, 0, 0], [1, 0, 0]]])
+    filt = hpfilter.Filter(coefficients=coeff, state=None)
+    npt.assert_array_equal(filt._coefficients, coeff)
+    assert filt._state is None
 
 
-def test_filter_init_state():
-    coeff = [[1, 1, 0], [1, 0, 0]]
-    zi = [1., 0.]
-    filt = dsp.Filter(coeff, state=zi)
-    npt.assert_array_equal(filt._state, zi)
+def test_filter_init_with_state():
+    coeff = np.array([[[1, 0, 0], [1, 0, 0]]])
+    state = np.array([[[1, 0]]])
+    filt = hpfilter.Filter(coefficients=coeff, state=state)
+    npt.assert_array_equal(filt._coefficients, coeff)
+    npt.assert_array_equal(filt._state, state)
 
 
-def test_reset():
-    coeff = [[1, 1, 0], [1, 0, 0]]
-    zi = [1., 0.]
-    filt = dsp.Filter(coeff, state=zi)
-    filt.reset()
-    npt.assert_array_equal(filt._state, [0., 0.])
+def test_filter_iir_init():
+    coeff = np.array([[1, 1/2, 0], [1, 0, 0]])
+    filt = hpfilter.FilterIIR(coeff)
+    npt.assert_array_equal(filt._coefficients, coeff[np.newaxis])
+
+
+def test_filter_sos_init():
+    sos = np.array([[1, 1/2, 0, 1, 0, 0]])
+    filt = hpfilter.FilterSOS(sos)
+    npt.assert_array_equal(filt._coefficients, sos[np.newaxis])
+
+
+def test_filter_sos_process():
+    sos = np.array([[1, 1/2, 0, 1, 0, 0]])
+    filt = hpfilter.FilterSOS(sos)
+    filt._state = np.array([[[0, 0]]])
+
+    sr = 4e3
+    times = np.arange(0, 2**10)/sr
+    sig = Signal(np.sin(2*np.pi*1e3*times), sr, signal_type='power')
+
+    filt.process(sig)
+
+    a = 1
+
+# def test_filter_init_state_empty():
+#     coeff = [[1, 0, 0], [1, 0, 0]]
+#     filt = hpfilter.Filter(coeff)
+#     npt.assert_array_equal(filt._state, np.zeros(2))
+
+
+# def test_filter_init_state():
+#     coeff = [[1, 1, 0], [1, 0, 0]]
+#     zi = [1., 0.]
+#     filt = hpfilter.Filter(coeff, state=zi)
+#     npt.assert_array_equal(filt._state, zi)
+
+
+# def test_reset():
+#     coeff = [[1, 1, 0], [1, 0, 0]]
+#     zi = [1., 0.]
+#     filt = hpfilter.Filter(coeff, state=zi)
+#     filt.reset()
+#     npt.assert_array_equal(filt._state, [0., 0.])
+
+
+def test_atleast_3d_first_dim():
+    arr = np.array([1, 0, 0])
+    desired = np.array([[[1, 0, 0]]])
+
+    arr_3d = hpfilter.atleast_3d_first_dim(arr)
+    npt.assert_array_equal(arr_3d, desired)
+    arr = np.array([[1, 0, 0], [2, 2, 2]])
+
+    desired = np.array([[[1, 0, 0], [2, 2, 2]]])
+    arr_3d = hpfilter.atleast_3d_first_dim(arr)
+    npt.assert_array_equal(arr_3d, desired)
+
+    arr = np.ones((2, 3, 5))
+    desired = arr.copy()
+    arr_3d = hpfilter.atleast_3d_first_dim(arr)
+    npt.assert_array_equal(arr_3d, desired)
