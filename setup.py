d1ad6ae55ae45169d69fffd9104ce5319e3ba4a9
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-11-18 18:39:20 +0100

    update classifiers for required python versions

--- setup.py
+++ setup.py
@@ -43,9 +43,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8'
     ],
     description="Project for data formats in acoustics.",
     install_requires=requirements,
