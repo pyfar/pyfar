88b429d68ce3fe92fc1245493f3c3653b94cf299
sikersten <simon.kersten@rwth-aachen.de>
2020-12-14 16:53:41 +0100

    Added test_copy

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,7 +1,10 @@
 import pyfar
+import numpy as np
+import numpy.testing as npt
 
 
 def test_copy():
+    """ Test copy method used by several classes."""
     obj_list = [pyfar.Signal(1000, 44100),
                 pyfar.Orientations(),
                 pyfar.Coordinates(),
@@ -10,13 +13,17 @@ def test_copy():
     for obj in obj_list:
         # Create copy
         obj_copy = obj.copy()
-        # Test for class
+        # Check class
         assert isinstance(obj_copy, obj.__class__)
-        # Test for location in memory
+        # Check ID
         assert id(obj) != id(obj_copy)
-        # Test for properties
-        for key, value in enumerate(obj_copy.__dict__)
-        
-    
-
+        # Check attributes
+        assert len(obj.__dict__) == len(obj_copy.__dict__)
+        for key, value in obj.__dict__.items():
+            assert key in obj_copy.__dict__.keys()
+            value_copy = obj_copy.__dict__[key]
+            if isinstance(value, np.ndarray):
+                npt.assert_allclose(value, value_copy)
+            else:
+                assert value == value_copy
 
