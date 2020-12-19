9b47410cf1df6f6aef8a9ac2d875dbcfdca54e02
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2020-12-19 09:15:02 +0100

    test for nominal center frequencies

---
+++ tests/test_fractional_octaves.py
@@ -0,0 +1,20 @@
+import numpy as np
+from numpy import testing as npt
+
+from pyfar.dsp import filter
+
+
+def test_center_frequencies_iec():
+    nominal_octs = [31.5, 63, 125, 250, 500, 1000, 2000, 4000, 8000, 16000]
+    actual_octs = filter.center_frequencies_fractional_octaves(num_fractions=1)
+    actual_octs_nom = actual_octs[0]
+    npt.assert_allclose(actual_octs_nom, nominal_octs)
+
+    nominal_thirds = [
+        25, 31.5, 40, 50, 63, 80, 100, 125, 160, 200, 250, 315, 400, 500, 630,
+        800, 1000, 1250, 1600, 2000, 2500, 3150, 4000, 5000, 6300, 8000, 10000,
+        12500, 16000, 20000]
+    actual_thirds = filter.center_frequencies_fractional_octaves(
+        num_fractions=3)
+    actual_thirds_nom = actual_thirds[0]
+    npt.assert_allclose(actual_thirds_nom, nominal_thirds)
