f5f3d373f529ace9a6e7863058dd0145f123d8fe
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-01-20 13:47:55 +0100

    center freqs: extend test coverage

--- tests/test_fractional_octaves.py
+++ tests/test_fractional_octaves.py
@@ -21,18 +21,21 @@ def test_center_frequencies_iec():
     npt.assert_allclose(actual_thirds_nom, nominal_thirds)
 
     with pytest.raises(ValueError, match="lower and upper limit"):
-        filter.center_frequencies_fractional_octaves(f_lims=(1,))
+        filter.center_frequencies_fractional_octaves(frequency_range=(1,))
 
     with pytest.raises(ValueError, match="lower and upper limit"):
-        filter.center_frequencies_fractional_octaves(f_lims=(3, 4, 5))
+        filter.center_frequencies_fractional_octaves(frequency_range=(3, 4, 5))
 
     with pytest.raises(
             ValueError, match="second frequency needs to be higher"):
         filter.center_frequencies_fractional_octaves(
-            f_lims=(8e3, 1e3))
+            frequency_range=(8e3, 1e3))
+
+    with pytest.raises(ValueError, match="Number of fractions can only be"):
+        filter.center_frequencies_fractional_octaves(5)
 
     actual_octs = filter.center_frequencies_fractional_octaves(
-        num_fractions=1, f_lims=(100, 4e3))
+        num_fractions=1, frequency_range=(100, 4e3))
     actual_octs_nom = actual_octs[0]
     nominal_octs_part = [125, 250, 500, 1000, 2000, 4000]
     npt.assert_allclose(actual_octs_nom, nominal_octs_part)
