bb6a486ae8412709db7104fdb9de7b14a5267854
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2021-01-25 18:12:03 +0100

    improved output of plot.utils.shortcuts()

--- pyfar/plot/utils.py
+++ pyfar/plot/utils.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 import os
 import json
 import pyfar.plot._line as _line
+from pyfar.plot.interaction import PlotParameter
 
 
 def plotstyle(style='light'):
@@ -78,7 +79,8 @@ def shortcuts(show=True):
     """Show and return keyboard shortcuts for interactive figures.
 
     Note that shortcuts are only available if using an interactive backend in
-    Matplotlib, e.g., by typing `%matplotlib qt`.
+    Matplotlib, e.g., by typing `%matplotlib qt`. Shortcuts can be customized
+    by edition 'shortcuts/shortcuts.json'. See below for the default shortcuts.
 
     Parameters
     ----------
@@ -91,6 +93,55 @@ def shortcuts(show=True):
     short_cuts : dict
         dictionary that contains all the shortcuts
 
+    Use these shortcuts to show different plots
+    -------------------------------------------
+    1: line.time
+    2: line.freq
+    3: line.phase
+    4: line.group_delay
+    5: line.spectrogram
+    6: line.time_freq
+    7: line.freq_phase
+    8: line.freq_group_delay
+
+    Use these shortcuts to control the plot
+    ---------------------------------------
+    left: move x-axis view to the left
+    right: move x-axis view to the right
+    up: move y-axis view upwards
+    down: y-axis view downwards
+    +: move colormap range up
+    -: move colormap range down
+    shift+right: zoom in x-axis
+    shift+left: zoom out x-axis
+    shift+up: zoom out y-axis
+    shift+down: zoom in y-axis
+    *: zoom colormap range in
+    _: zoom colormap range out
+    x: toggle between linear and logarithmic x-axis
+    y: toggle between linear and logarithmic y-axis
+    c: toggle between linear and logarithmic color data
+    a: toggle between plotting all channels and plottinng single channels
+    .: show next channel
+    ,: show previous channel
+
+    Notes on plot controls
+    ----------------------
+    Moving and zooming the x and y axes is supported by all plots.
+
+    Moving and zooming the colormap is only supported by plots that have a
+    colormap.
+
+    Toggling the x-axis is supported by: line.time, line.freq, line.phase,
+    line.group_delay, line.spectrogram, line.time_freq, line.freq_phase,
+    line.freq_group_delay
+
+    Toggling the y-axis is supported by: line.time, line.freq, line.phase,
+    line.group_delay, line.spectrogram, line.time_freq, line.freq_phase,
+    line.freq_group_delay
+
+    Toggling the colormap is supported by: line.spectrogram
+
     """
     # load short cuts from json file
     sc = os.path.join(os.path.dirname(__file__), 'shortcuts', 'shortcuts.json')
@@ -99,13 +150,49 @@ def shortcuts(show=True):
 
     # print list of short cuts
     if show:
-        print("Use these shortcuts to show different plots:")
+        # get list of plots that allow toogling axes and colormaps
+        x_toggle = []
+        y_toggle = []
+        cm_toggle = []
         for plot in short_cuts["plots"]:
-            print(f'{plot}: {short_cuts["plots"][plot]}')
+            params = PlotParameter(plot)
+            if params.x_type is not None:
+                if len(params.x_type) > 1:
+                    x_toggle.append(plot)
+            if params.y_type is not None:
+                if len(params.y_type) > 1:
+                    y_toggle.append(plot)
+            if params.cm_type is not None:
+                if len(params.cm_type) > 1:
+                    cm_toggle.append(plot)
+
+        # print information
+        print("Use these shortcuts to show different plots")
+        print("-------------------------------------------")
+        plt = short_cuts["plots"]
+        for p in plt:
+            print(f'{", ".join(plt[p]["key"])}: {p}')
         print(" ")
-        print("Use these shortcuts to control the plot:")
+
+        print("Use these shortcuts to control the plot")
+        print("---------------------------------------")
         ctr = short_cuts["controls"]
         for action in ctr:
-            print(f'{ctr[action]["key"]}: {ctr[action]["info"]}')
+            print(f'{", ".join(ctr[action]["key"])}: {ctr[action]["info"]}')
+        print(" ")
+
+        print("Notes on plot controls")
+        print("----------------------")
+        print("Moving and zooming the x and y axes is supported by all plots.")
+        print(" ")
+        print(("Moving and zooming the colormap is only supported by plots "
+               "that have a colormap."))
+        print(" ")
+        print(f"Toggling the x-axis is supported by: {', '.join(x_toggle)}")
+        print(" ")
+        print(f"Toggling the y-axis is supported by: {', '.join(y_toggle)}")
+        print(" ")
+        print(f"Toggling the colormap is supported by: {', '.join(cm_toggle)}")
+        print(" ")
 
     return short_cuts
