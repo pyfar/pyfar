4f8578b5a3a2b4535780290fe0827b0d6b316313
Kai Jurgeit <kai.jurgeit@t-online.de>
2021-01-25 11:20:28 +0100

    Added line at gitignore EOF, conftest and stub_utils.

---
+++ tests/conftest.py
@@ -0,0 +1,11 @@
+import pytest
+import numpy as np
+import os.path
+import sofa
+import scipy.io.wavfile as wavfile
+
+from pyfar.orientations import Orientations
+
+# import stub_utils
+
+
---
+++ tests/stub_utils.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+from unittest import mock
+
+from pyfar import Signal
+
+
+def saved_objects_stub():
+    pass
