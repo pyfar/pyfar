20caf8830d86c3e281ae85b3ee9e95060b925aeb
Lukas Hartmann <lukasfh94@gmail.com>
2021-02-02 09:33:31 +0100

    First normalize implementation

--- pyfar/dsp/__init__.py
+++ pyfar/dsp/__init__.py
@@ -1,7 +1,7 @@
 from .classes import (Filter, FilterFIR, FilterIIR, FilterSOS)
 from .dsp import (
     phase, group_delay, wrap_to_2pi, nextpow2, spectrogram)
-
+from .normalize import(normalize)
 
 __all__ = [
     Filter, FilterFIR, FilterIIR, FilterSOS,
