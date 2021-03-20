5e929f787132592b5dd193ece7a4b17536462a38
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-03-20 13:52:24 +0100

    wip: temporarily disable copy utils test, needs to be moved as copy utils was removed

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,24 +1,24 @@
-import pyfar
+# import pyfar
 
 
-def test_copy(sphericalvoronoi, timedata, frequencydata):
-    """ Test copy method used by several classes."""
-    obj_list = [pyfar.Signal(1000, 44100),
-                pyfar.Orientations(),
-                pyfar.Coordinates(),
-                pyfar.dsp.Filter(),
-                sphericalvoronoi,
-                timedata,
-                frequencydata]
+# def test_copy(sphericalvoronoi, timedata, frequencydata):
+#     """ Test copy method used by several classes."""
+#     obj_list = [pyfar.Signal(1000, 44100),
+#                 pyfar.Orientations(),
+#                 pyfar.Coordinates(),
+#                 pyfar.dsp.Filter(),
+#                 sphericalvoronoi,
+#                 timedata,
+#                 frequencydata]
 
-    for obj in obj_list:
-        # Create copy
-        obj_copy = obj.copy()
-        # Check class
-        assert isinstance(obj_copy, obj.__class__)
-        # Check ID
-        assert id(obj) != id(obj_copy)
-        # Check attributes
-        assert len(obj.__dict__) == len(obj_copy.__dict__)
-        # Check if copied objects are equal
-        assert obj_copy == obj
+#     for obj in obj_list:
+#         # Create copy
+#         obj_copy = obj.copy()
+#         # Check class
+#         assert isinstance(obj_copy, obj.__class__)
+#         # Check ID
+#         assert id(obj) != id(obj_copy)
+#         # Check attributes
+#         assert len(obj.__dict__) == len(obj_copy.__dict__)
+#         # Check if copied objects are equal
+#         assert obj_copy == obj
