00a4e0315d14ca41797e373d1685ef319035941b
udowa <u.waldeck@outlook.com>
2021-04-03 11:46:33 +0200

    WIP pull request feedback

--- pyfar/dsp/__init__.py
+++ pyfar/dsp/__init__.py
@@ -1,11 +1,10 @@
 from .classes import (Filter, FilterFIR, FilterIIR, FilterSOS)
-from .dsp import (phase, group_delay, wrap_to_2pi, nextpow2, spectrogram)
-from .window import (rect, hann, hamming, blackman, bartlett, kaiser, 
-                     kaiserBessel, flattop, dolphChebychev, window)
+from .dsp import (phase, group_delay, wrap_to_2pi,
+                  nextpow2, spectrogram, windows)
+
 
 __all__ = [
     Filter, FilterFIR, FilterIIR, FilterSOS,
     phase, group_delay, wrap_to_2pi, nextpow2, spectrogram,
-    rect, hann, hamming, blackman, bartlett, kaiser, kaiserBessel, flattop, 
-    dolphChebychev, window
+    windows
 ]
--- pyfar/dsp/dsp.py
+++ pyfar/dsp/dsp.py
@@ -158,7 +158,7 @@ def spectrogram(signal, dB=True, log_prefix=20, log_reference=1,
         Times in seconds at which the magnitude spectrum was computed
     spectrogram : numpy array
     """
-
+    
     # check input
     if not isinstance(signal, Signal):
         raise TypeError('Input data has to be of type: Signal.')
@@ -171,8 +171,8 @@ def spectrogram(signal, dB=True, log_prefix=20, log_reference=1,
     window = sgn.get_window(window, window_length)
 
     frequencies, times, spectrogram = sgn.spectrogram(
-            x=signal.time.squeeze(), fs=signal.sampling_rate, window=window,
-            noverlap=window_overlap, mode='magnitude', scaling='spectrum')
+        x=signal.time.squeeze(), fs=signal.sampling_rate, window=window,
+        noverlap=window_overlap, mode='magnitude', scaling='spectrum')
 
     # remove normalization from scipy.signal.spectrogram
     spectrogram /= np.sqrt(1 / window.sum()**2)
@@ -187,3 +187,102 @@ def spectrogram(signal, dB=True, log_prefix=20, log_reference=1,
     times -= times[0]
 
     return frequencies, times, spectrogram
+
+
+def windows(signal, window_function='hann', times=None,
+            window_shape='symmetric', unit='samples', **kwargs):
+    """Returns a windowed pyfar signal with selected window_function.
+
+    Parameters
+    ----------
+    signal : Signal object
+        An audio signal object from the pyfar signal class
+
+    window_function: string
+        select window type from 'rect', 'hann', 'hamming', 'blackman',
+        'bartlett', 'kaiser', 'kaiserBessel' , 'flattop' or 'dolphChebychev'
+
+    times: int
+        sets window length in unit('samples') of selected window
+
+    window_shape: string
+        'symmetric' (default), 'left', 'right'
+
+    Returns
+    -------
+    out : pyfar
+        window coefficients
+    """
+
+    # check input and default values
+    if not isinstance(signal, Signal):
+        raise TypeError('Input data has to be of type: Signal.')
+
+    if not isinstance(times, int):
+        raise TypeError('times has to be of type int.')
+
+    window_shape_list = ['symmteric', 'left', 'right']
+    if window_shape not in window_shape_list:
+        raise ValueError(f"window_shape is {window_shape} but has to be"
+                         f" one of the following:"
+                         f" {', '.join(list(window_shape_list))}.")
+
+    if not isinstance(unit, str):
+        raise TypeError('''window_shape has to be a sting of the following:
+                        'samples', 's','ms','mus'.''')
+    # copy signal object
+    signal_copy = signal.copy()
+
+    if times is None:
+        times_left = 0
+        times_right = signal_copy.n_samples
+    elif len(times) = 2:
+        times_left = times(1)
+        times_right = times(2)
+
+    # copy signal object
+    signal_copy = signal.copy()
+    # create selected window with simulated switch case
+    switcher_window = {
+        'rect': sgn.windows.boxcar,
+        'hann': sgn.windows.hann,
+        'hamming': sgn.windows.hamming,
+        'blackman': sgn.windows.blackman,
+        'bartlett': sgn.windows.bartlett,
+        'kaiser': sgn.windows.kaiser,
+        'flattop': sgn.windows.flattop,
+        'chebwin': sgn.windows.chebwin,
+    }
+    if window_function in switcher_window:
+        win = switcher_window[window_function](times, **kwargs)
+    else:
+        raise ValueError(f"window_function is {window_function} but has to be"
+                         f" one of the following:"
+                         f" {', '.join(list(switcher_window))}.")
+
+    # apply windowing to time domain copy of signal
+    # if times > np.size(signal_copy.time[..., :]):
+    #     ValueError('>>>>>>> window is longer than signal!')
+
+    # create zeropadded window with shape of signal
+    windowShape = np.zeros(np.size(signalCopyFlat.time[0]))
+
+    # check if window is not being set within signal bounds
+    if windowStartIndex+np.size(win) <= np.size(windowShape):
+        windowShape[windowStartIndex:windowStartIndex+np.size(win)] = win
+    elif windowStartIndex+np.size(win)-np.size(windowShape) >= np.size(win)-1:
+        # return not windowed signal with warning
+        signalCopy = signalCopyFlat.reshape(signal.cshape)
+        print('>>>>>>> No windowing applied!')
+        return signalCopy
+    else:
+        # apply window only partially if windowStartIndex set, so that the
+        # window doesn't fit at the end
+        win = win[:-(windowStartIndex+np.size(win)-np.size(windowShape))]
+        windowShape[windowStartIndex:windowStartIndex+np.size(win)] = win
+
+    # apply window
+    signalCopyFlat.time = signalCopyFlat.time * windowShape
+    signalCopy = signalCopyFlat.reshape(signal.cshape)
+
+    return signalCopy
