f529586282d9f6fbd714efc7eb52553f0438247c
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-04-10 20:57:47 +0200

    move filter objects module to classes folder

--- pyfar/classes/__init__.py
+++ pyfar/classes/__init__.py
@@ -1,10 +1,13 @@
 from .signal import Signal, TimeData, FrequencyData
 from .coordinates import Coordinates
 from .orientations import Orientations
+from .filter import FilterFIR, FilterIIR, FilterSOS
 
 from . import signal
 from . import coordinates
 from . import orientations
+from . import filter
+
 
 __all__ = [
     'signal',
@@ -15,4 +18,8 @@ __all__ = [
     'FrequencyData',
     'Coordinates',
     'Orientations',
+    'FilterFIR',
+    'FilterIIR',
+    'FilterSOS',
+    'filter'
 ]
