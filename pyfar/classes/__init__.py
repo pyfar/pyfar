370a979e41382d0128cf961ecf4bfc328ca22deb
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2021-04-10 20:39:40 +0200

    inits for class modules, required for things like arithmetic operations

--- pyfar/classes/__init__.py
+++ pyfar/classes/__init__.py
@@ -2,7 +2,14 @@ from .signal import Signal, TimeData, FrequencyData
 from .coordinates import Coordinates
 from .orientations import Orientations
 
+from . import signal
+from . import coordinates
+from . import orientations
+
 __all__ = [
+    'signal',
+    'coordinates',
+    'orientations',
     'Signal',
     'TimeData',
     'FrequencyData',
