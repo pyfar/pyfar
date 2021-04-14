d5166f766ac546d894e65b7e7281a2b94a46015a
Stephan Eckes <eckes@mailbox.org>
2021-04-14 11:54:27 +0200

    update zeropadding

--- pyfar/dsp/__init__.py
+++ pyfar/dsp/__init__.py
@@ -2,8 +2,10 @@ from .classes import (Filter, FilterFIR, FilterIIR, FilterSOS)
 from .dsp import (
     phase, group_delay, wrap_to_2pi, nextpow2, spectrogram)
 
+from .zeropadding import (zeropadding)
+
 
 __all__ = [
     Filter, FilterFIR, FilterIIR, FilterSOS,
-    phase, group_delay, wrap_to_2pi, nextpow2, spectrogram
+    phase, group_delay, wrap_to_2pi, nextpow2, spectrogram, zeropadding
 ]
--- pyfar/dsp/zeropadding.py
+++ pyfar/dsp/zeropadding.py
@@ -1,9 +1,10 @@
 from pyfar import Signal
 import numpy as np
 
-def zeropadding(signal, count, pos = 'front'):
+
+def zeropadding(signal, count, pos='front'):
     """Returns a time signal of type Signal padded with zeros
-    
+
     Parameters
     ----------
     signal: Signal
@@ -17,24 +18,26 @@ def zeropadding(signal, count, pos = 'front'):
     -------
     padded_signal : Signal
         The input signal with a zero padded time domain.
-    
+
     """
     if(type(signal) != Signal):
         assert("signal must be of type Signal")
-    
+
     signal_flat = signal.flatten()
 
     new_length = signal.n_samples + count
-    padded_signal = Signal(np.zeros([signal_flat.cshape[0], new_length]),\
-                            signal.sampling_rate)
+    padded_signal = Signal(np.zeros([signal_flat.cshape[0], new_length]),
+                           signal.sampling_rate)
 
     for channel in range(0, signal_flat.cshape[0]):
         pos.lower()
         if pos == 'front':
-            padded_signal.time[channel][count:] = signal_flat.time[channel].copy()
-        
+            padded_signal.time[channel][count:] = \
+                signal_flat.time[channel].copy()
+
         elif pos == 'back':
-            padded_signal.time[channel][:-count] = signal_flat.time[channel].copy()
+            padded_signal.time[channel][:-count] = \
+                signal_flat.time[channel].copy()
 
         else:
             assert("position must be 'front' or 'back'")
