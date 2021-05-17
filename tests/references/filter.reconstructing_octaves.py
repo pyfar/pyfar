01c215a68d1b60a513018e9d5256a8993b5089dd
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2021-05-17 18:06:19 +0200

    flake8 fix

--- tests/references/filter.reconstructing_octaves.py
+++ tests/references/filter.reconstructing_octaves.py
@@ -12,7 +12,7 @@ np.savetxt("filter.reconstructing_octaves_1_0.csv", y.time)
 
 # overlap=0, slope=0
 y, _ = pf.dsp.filter.reconstructing_fractional_octave_bands(
-    x,frequency_range=(8e3, 16e3), overlap=0, slope=0, n_samples=2**10)
+    x, frequency_range=(8e3, 16e3), overlap=0, slope=0, n_samples=2**10)
 np.savetxt("filter.reconstructing_octaves_0_0.csv", y.time)
 
 # overlap=1, slope=3
