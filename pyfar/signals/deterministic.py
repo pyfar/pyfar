7a35a41cccef7b551f8acc0f1e51b3620e90d814
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2021-06-02 12:59:16 +0200

    added docstring for discussion

--- pyfar/signals/deterministic.py
+++ pyfar/signals/deterministic.py
@@ -143,6 +143,14 @@ def linear_sweep(n_samples, frequency_range, n_fade_out=90, amplitude=1,
     seconds, and the frequency limits :math:`f_\\mathrm{low}` and
     :math:`f_\\mathrm{high}`.
 
+    .. note::
+        The linear sweep can also be generated in the frequency domain
+        (see :py:func:`~general_sweep_synthesis`). Time domain synthesis
+        exhibits a constant temporal envelope in trade of slight ripples in the
+        magnitude response. Frequency domain synthesis exhibits smooth
+        magnitude spectra and in trade of a slightly irregular temporal
+        envelope.
+
     Parameters
     ----------
     n_samples : int
@@ -200,6 +208,14 @@ def exponential_sweep(n_samples, frequency_range, n_fade_out=90, amplitude=1,
     seconds, and the frequency limits :math:`f_\\mathrm{low}` and
     :math:`f_\\mathrm{high}`.
 
+    .. note::
+        The exponential sweep can also be generated in the frequency domain
+        (see :py:func:`~general_sweep_synthesis`). Time domain synthesis
+        exhibits a constant temporal envelope in trade of slight ripples in the
+        magnitude response. Frequency domain synthesis exhibits smooth
+        magnitude spectra and in trade of a slightly irregular temporal
+        envelope.
+
     Parameters
     ----------
     n_samples : int
@@ -240,6 +256,120 @@ def exponential_sweep(n_samples, frequency_range, n_fade_out=90, amplitude=1,
         "exponential", sweep_rate)
 
     return signal
+
+
+def general_sweep_synthesis(
+    n_samples, spectrum, start_margin=None, stop_margin=None,
+    requency_range=None, double=True, sampling_rate=44100):
+    """
+    Frequency domain sweep synthesis with arbitrary magnitude response.
+
+    TODO Link to fractional octave smoothing in Notes
+
+    TODO Implement calculation of group delay of impulse responses
+
+    TODO Examples
+
+    Sweep sweep synthesis according to [#]_
+
+    .. note::
+        The linear and exponential sweep can also be generated in the time
+        domain (see :py:func:`~linear_sweep`, :py:func:`~exponential_sweep`).
+        Frequency domain synthesis exhibits smooth magnitude spectra and in
+        trade of a slightly irregular temporal envelope. Time domain synthesis
+        exhibits a constant temporal envelope in trade of slight ripples in the
+        magnitude response.
+
+    Parameters
+    ----------
+    n_samples : int
+        The length of the sweep in samples.
+    spectrum : Signal, string
+        Specify the magnitude response of the sweep.
+
+        signal
+            The magnitude response as :py:class:`~pyfar.classes.audio.Signal`
+            object. If ``signal.n_samples`` is smaller than `n_samples`, zeros
+            are padded to the end of `signal`. Note that `frequency_range` is
+            not required in this case.
+        ``'linear'``
+            Design a sweep with linearly increasing frequency and a constant
+            magnitude spectrum.
+        ``'exponential'``
+            Design a sweep with exponentially increasing frequency. The
+            magnitude decreases by 3 dB per frequency doubling and has constant
+            energy in fiters of relative constant bandwidth (e.g. octaves).
+        ``'perfect'``
+            Perfect sweep according to [#]_. Note that the parameters
+            `start_margin`, `stop_margin`, `frequency_range` and `double` are
+            not required in this case.
+
+    start_margin : int, optional
+        The time in samples, at which the sweep starts. The start margin is
+        required because the frequency domain sweep synthesis has pre-ringing
+        in the time domain. Not required if `spectrum` is ``'perfect'``.
+    stop_margin : int, optional
+        Time in samples, at which the sweep stops. This is relative to
+        `n_samples`, e.g., a stop margin of 100 samples means that the sweep
+        ends at sample ``n_samples-10``. This is required, because the
+        frequency domain sweep synthesis has post-ringing in the time domain.
+        Not required if `spectrum` is ``'perfect'``.
+    frequency_range : array like, optional
+        Frequency range of the sweep given by the lower and upper cut-off
+        frequency in Hz. The restriction of the frequency range is realized
+        by appling 8th order Butterworth filters at the specified frequencies.
+        Not required if `spectrum` is ``'perfect'`` or `signal`.
+    double : bool, optional
+        Double `n_samples` during the sweep calculation (recommended). The
+        default is  ``True``. Not required if `spectrum` is ``'perfect'``.
+    sampling_rate : int, optional
+        The sampling rate in Hz. The default is ``44100``.
+
+    Returns
+    -------
+    sweep : Signal
+        The sweep signal. The Signal is in the time domain and has the ``none``
+        FFT normalization (see :py:func:`~pyfar.dsp.fft.normalization`). The
+        sweep parameters are written to `comment`.
+    group_delay_sweep : FrequencyData
+        The group delay of the sweep as a single sided spectrum between 0 Hz
+        and ``sampling_rate/2``.
+
+        TODO add this after implementation is complete:
+
+        This can be used to calculate the group delay of the impulse responses
+        of linear and harmonic distortion products after deconvoloution (see
+        :py:func:`~pyfar.dsp...`).
+
+    Notes
+    -----
+    The envelope of the sweep time signal should be constant, appart from
+    slight overshoots at the beginning and end. If this is not the case, try to
+    provide a smoother spectrum (if `spectrum` is `signal`) or increase
+    `n_samples`.
+
+    References
+    ----------
+    .. [#] S. Müller, P. Massarani. 'Transfer Function Measurement with Sweeps.
+           Directors Cut Including Previously Unreleased Material and some
+           Corrections. J. Audio Eng. Soc. 2001, 49 (6), 443–471.
+    .. [#] C. Antweiler, A. Telle, P. Vary, G. Enzner. 'Perfect-Sweep NLMS for
+           Time-Variant Acoustic System Identification,' IEEE Int. Conf.
+           Acoustics, Speech and Signal Processing (ICASSP),
+           Prague, Czech Republic, 2011. doi: 10.1109/ICASSP.2012.6287930.
+
+    Examples
+    --------
+    TODO Example with spectrum=singal
+          (e.g., Bass emphasis by means of low shelve filter)
+
+    TODO Examples with spectrum="linear"
+
+    TODO Examples with spectrum="exponential"
+
+    TODO Examples with spectrum="perfect"
+    """
+    pass
 
 
 def _time_domain_sweep(n_samples, frequency_range, n_fade_out, amplitude,
