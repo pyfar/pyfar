226b3d40b35a8fbce148a9bc7103ac5ee6526955
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2021-11-19 15:17:41 +0100

    fix import in tests

--- tests/test_convolution.py
+++ tests/test_convolution.py
@@ -1,13 +1,13 @@
 import numpy as np
 from pyfar import Signal
-from pyfar.dsp import convolution
+from pyfar.dsp import convolve_overlap_add
 
 
 def test_overlap_add(impulse):
     ir = [1, 0.5, 0.25, 0, 0, 0, 0, 0]
     ir_sig = Signal(ir, impulse.sampling_rate)
 
-    res = convolution.convolve_overlap_add(impulse, ir_sig, mode='full')
+    res = convolve_overlap_add(impulse, ir_sig, mode='full')
 
     desired = np.zeros((1, impulse.n_samples + len(ir) - 1))
     desired[:, :len(ir)] = ir
