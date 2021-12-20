923023cb5c6d7c845d4810229fc8bcbafd2e7faf
Alexander Deutsch <alex.deutsch93@web.de>
2021-12-20 10:56:07 +0100

    [skip ci] Add formulas for fft normalisation

--- pyfar/_concepts/fft_concepts/normalization.py
+++ pyfar/_concepts/fft_concepts/normalization.py
@@ -3,6 +3,37 @@ pyfar implements five normalization [1]_ that can be applied to spectra. The
 normalizations are available from :py:func:`~pyfar.dsp.fft.normalization`.
 Note that the time signals do not change regardless of the normalization.
 
+
+amplitude spectrum
+
+.. math::
+        \overline{X}(k) = \frac{1}{N} X(k)
+
+
+power spectrum
+
+.. math::
+        \overline{\overline{X}}(k) = \lvert \overline{X}(k) \lvert ^2 =
+        \frac{1}{N^2} \lvert X(k) \lvert ^2
+
+
+power spectral density (PSD) or power desity spectrum
+
+.. math::
+        \overline{\overline{\underline{X}}}(k) = \frac{N}{f_s}
+        \overline{\overline{X}}(k) = \frac{N}{f_s} \lvert \overline{X}(k)
+        \lvert ^2 = \frac{1}{f_s N} \lvert  X(k) \lvert ^2
+
+
+RMS spectrum
+
+.. math::
+        \overline{X}_{RMS}(k) = \left\{\begin{array}{ll}
+        \frac{1}{\sqrt{2}} \overline{X}_{\text{SS}}(k) \quad \forall 0<k<
+        \frac{N}{2}\\ \quad \; \overline{X}_{\text{SS}}(k) \quad \forall k=0,
+        k= \frac{N}{2} \end{array}\right. .
+
+
 References
 ----------
 .. [1]  J. Ahrens, C. Andersson, P. Höstmad, and W. Kropp, “Tutorial on
