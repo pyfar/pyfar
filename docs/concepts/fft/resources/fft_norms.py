1b88a3e3f3c9e59f5f2fc58c51bad3d06c218577
Simon Kersten <kersten@ITA.AKUSTIK.RWTH-AACHEN.DE>
2022-01-11 12:14:57 +0100

    [skip ci] wip: finished examples

--- docs/concepts/fft/resources/fft_norms.py
+++ docs/concepts/fft/resources/fft_norms.py
@@ -18,234 +18,40 @@ sine = pf.signals.sine(1e3, n_samples, sampling_rate=sampling_rate)
 noise = pf.signals.noise(
     n_samples, rms=1/np.sqrt(2), sampling_rate=sampling_rate)
 
-# %% Non squared norms
-plt.close('all')
+# %% Plot
 fft_norms = ['none', 'unitary', 'amplitude', 'rms', 'power', 'psd']
 ylim = [(-23, 63), (-23, 63), (-83, 3), (-83, 3), (-83, 3), (-83, 3)]
-units = ['\\text{V/Hz}', '\\text{V/Hz}', '\\text{V}', '\\text{V}', '\\text{V}', 'V/\\sqrt{\\text{Hz}}']
+units = [
+    '$\\mathrm{V/Hz}$', '$\\mathrm{V/Hz}$', '$\\mathrm{V}$', '$\\mathrm{V}$',
+    '$\\mathrm{V}$', '$V/\\sqrt{\\mathrm{Hz}}$']
 log_prefixs = [20, 20, 20, 20, 10, 10]
 
-fig, axes = plt.subplots(int(len(fft_norms)/2), 2, figsize=(30/2.54, 30/2.54), sharex=True)
-for idx, (fft_norm, unit, log_prefix, ylim) in enumerate(zip(fft_norms, units, log_prefixs, ylim)):
-    ax = plt.subplot(int(len(fft_norms)/2), 2, idx+1)
+fig, axes = plt.subplots(3, 2, figsize=(30/2.54, 30/2.54), sharex=True)
+for idx, (fft_norm, unit, log_prefix, ylim) in \
+        enumerate(zip(fft_norms, units, log_prefixs, ylim)):
+    ax = plt.subplot(3, 2, idx+1)
     filter.fft_norm = fft_norm
-    ir.fft_norm = fft_norm
+    impulse.fft_norm = fft_norm
     sine.fft_norm = fft_norm
     noise.fft_norm = fft_norm
-    pf.plot.freq(ir, label='IR Signal', log_prefix=log_prefix, ax=ax)
-    pf.plot.freq(sine, label='Sine', log_prefix=log_prefix, ax=ax)
-    pf.plot.freq(noise, label='Noise', log_prefix=log_prefix, ax=ax)
-    pf.plot.freq(filter, label='FIR Filter in dB re $'+unit.replace('V', '1')+'$', log_prefix=log_prefix, ls='--', ax=ax)
+    pf.plot.freq(
+        impulse, label='Impulse in dB re '+unit.replace('V', '1'),
+        log_prefix=log_prefix, ax=ax)
+    pf.plot.freq(
+        filter, label='FIR Filter in dB re '+unit.replace('V', '1'),
+        log_prefix=log_prefix, ax=ax)
+    pf.plot.freq(
+        noise, label=f'Noise in dB re {unit}', log_prefix=log_prefix, ax=ax)
+    pf.plot.freq(
+        sine, label=f'Sine in dB re {unit}', log_prefix=log_prefix, ax=ax)
+
     ax.set_ylim(ylim)
     ax.legend(loc='lower left')
-    ax.set_ylabel(f'Magnitude in dB re $1 {unit}$')
     if idx % 2 == 1:
         ax.yaxis.set_ticklabels([])
+        ax.set_ylabel('')
     if idx < 4:
         ax.set_xlabel('')
     ax.set_title('\''+fft_norm+'\'')
 fig.tight_layout()
 plt.savefig('fft_norms_examples.png', dpi=150)
-
-# %% All norms
-plt.close('all')
-fft_norms = ['none', 'unitary', 'amplitude', 'rms', 'power', 'psd']
-ylim = [(-23, 63), (-23, 63), (-83, 3), (-83, 3), (-153, 3), (-153, 3)]
-units = ['V/Hz', 'V/Hz', 'V', 'V', 'V', 'V/sqrt(Hz)']
-ylim = [(-23, 63), (-23, 63), (-83, 3), (-83, 3), (-153, 3), (-153, 3)]
-
-fig, axes = plt.subplots(3, 2, figsize=(30/2.54, 30/2.54), sharex=True)
-for idx, (fft_norm, unit, ylim) in enumerate(zip(fft_norms, units, ylim)):
-    ax = plt.subplot(3, 2, idx+1)
-    ir.fft_norm = fft_norm
-    sine.fft_norm = fft_norm
-    noise.fft_norm = fft_norm
-    pf.plot.freq(ir, label='Impulse Response', ax=ax)
-    pf.plot.freq(sine, label='Sine', ax=ax)
-    pf.plot.freq(noise, label='Noise', ax=ax)
-    ax.set_ylim(ylim)
-    ax.set_xlabel('')
-    ax.set_ylabel('Magnitude in ')
-    ax.set_title('\''+fft_norm+'\'')
-
-fig.tight_layout()
-
-# %% standard filters ---------------------------------------------------------
-_, ax = plt.subplots(2, 2, figsize=(30/2.54, 20/2.54),
-                     sharex=True, sharey=True)
-# high-pass
-N = 4
-frequency = 1e3
-btype = 'highpass'
-axis = ax[0, 0]
-y = pf.dsp.filter.butterworth(impulse, N, frequency, btype=btype)
-pf.plot.freq(y, ax=axis, label='Butterworth')
-
-y = pf.dsp.filter.bessel(impulse, N, frequency, btype=btype)
-pf.plot.freq(y * 10**(-5/20), ax=axis, label='Bessel')
-
-y = pf.dsp.filter.chebyshev1(impulse, N, 1, frequency, btype=btype)
-pf.plot.freq(y * 10**(-10/20), ax=axis, label='Chebyshev Type I')
-
-y = pf.dsp.filter.chebyshev2(impulse, N, 60, 300, btype=btype)
-pf.plot.freq(y * 10**(-15/20), ax=axis, label='Chebyshev Type II')
-
-y = pf.dsp.filter.elliptic(impulse, N, 1, 60, frequency, btype=btype)
-pf.plot.freq(y * 10**(-20/20), ax=axis, label='Elliptic')
-
-axis.set_title('High-pass filter')
-axis.set_xlabel('')
-axis.set_xlim(20, 20e3)
-axis.set_ylim(-95, 5)
-
-# low-pass
-N = 4
-frequency = 1e3
-btype = 'lowpass'
-axis = ax[0, 1]
-y = pf.dsp.filter.butterworth(impulse, N, frequency, btype=btype)
-pf.plot.freq(y, ax=axis, label='Butterworth')
-
-y = pf.dsp.filter.bessel(impulse, N, frequency, btype=btype)
-pf.plot.freq(y * 10**(-5/20), ax=axis, label='Bessel')
-
-y = pf.dsp.filter.chebyshev1(impulse, N, 1, frequency, btype=btype)
-pf.plot.freq(y * 10**(-10/20), ax=axis, label='Chebyshev Type I')
-
-y = pf.dsp.filter.chebyshev2(impulse, N, 60, 3500, btype=btype)
-pf.plot.freq(y * 10**(-15/20), ax=axis, label='Chebyshev Type II')
-
-y = pf.dsp.filter.elliptic(impulse, N, 1, 60, frequency, btype=btype)
-pf.plot.freq(y * 10**(-20/20), ax=axis, label='Elliptic')
-
-axis.set_title('Low-pass filter')
-axis.set_xlabel('')
-axis.set_xlim(20, 20e3)
-axis.set_ylim(-95, 5)
-
-# band-pass
-N = 4
-frequency = [500, 2e3]
-btype = 'bandpass'
-axis = ax[1, 0]
-y = pf.dsp.filter.butterworth(impulse, N, frequency, btype=btype)
-pf.plot.freq(y, ax=axis, label='Butterworth')
-
-y = pf.dsp.filter.bessel(impulse, N, frequency, btype=btype)
-pf.plot.freq(y * 10**(-5/20), ax=axis, label='Bessel')
-
-y = pf.dsp.filter.chebyshev1(impulse, N, 1, frequency, btype=btype)
-pf.plot.freq(y * 10**(-10/20), ax=axis, label='Chebyshev Type I')
-
-y = pf.dsp.filter.chebyshev2(impulse, N, 60, [175, 5500], btype=btype)
-pf.plot.freq(y * 10**(-15/20), ax=axis, label='Chebyshev Type II')
-
-y = pf.dsp.filter.elliptic(impulse, N, 1, 60, frequency, btype=btype)
-pf.plot.freq(y * 10**(-20/20), ax=axis, label='Elliptic')
-
-axis.set_title('Band-pass filter')
-axis.set_xlabel('')
-axis.set_xlim(20, 20e3)
-axis.set_ylim(-95, 5)
-
-# band-stop
-N = 4
-frequency = [250, 5e3]
-btype = 'bandstop'
-axis = ax[1, 1]
-y = pf.dsp.filter.butterworth(impulse, N, frequency, btype=btype)
-pf.plot.freq(y, ax=axis, label='Butterworth')
-
-y = pf.dsp.filter.bessel(impulse, N, frequency, btype=btype)
-pf.plot.freq(y * 10**(-5/20), ax=axis, label='Bessel')
-
-y = pf.dsp.filter.chebyshev1(impulse, N, 1, frequency, btype=btype)
-pf.plot.freq(y * 10**(-10/20), ax=axis, label='Chebyshev Type I')
-
-y = pf.dsp.filter.chebyshev2(impulse, N, 60, [650, 2e3], btype=btype)
-pf.plot.freq(y * 10**(-15/20), ax=axis, label='Chebyshev Type II')
-
-y = pf.dsp.filter.elliptic(impulse, N, 1, 60, frequency, btype=btype)
-pf.plot.freq(y * 10**(-20/20), ax=axis, label='Elliptic')
-
-axis.set_title('Band-stop filter')
-axis.set_xlabel('')
-axis.set_xlim(20, 20e3)
-axis.set_ylim(-95, 5)
-axis.legend(loc=3)
-
-plt.tight_layout()
-plt.savefig('filter_types_standard.png', dpi=150)
-
-# %% filter banks -------------------------------------------------------------
-
-_, ax = plt.subplots(1, 2, figsize=(30/2.54, 12/2.54), sharey=True)
-
-# DIN Filterbank
-axis = ax[0]
-y = pf.dsp.filter.fractional_octave_bands(impulse, 1, freq_range=(60, 12e3))
-pf.plot.freq(y, ax=axis)
-axis.set_title('Fractional octave bands (room acoustics)')
-axis.set_xlim(20, 20e3)
-axis.set_ylim(-60, 10)
-
-# Reconstructing Filterbank
-axis = ax[1]
-y, *_ = pf.dsp.filter.reconstructing_fractional_octave_bands(impulse, 1)
-pf.plot.freq(y, ax=axis)
-axis.set_title('Fractional octave bands (perfect reconstructing)')
-axis.set_xlim(20, 20e3)
-axis.set_ylim(-60, 10)
-
-plt.tight_layout()
-plt.savefig('filter_types_filterbanks.png', dpi=150)
-
-# %% cross-over ---------------------------------------------------------------
-_, ax = plt.subplots(1, 1, figsize=(15/2.54, 12/2.54))
-
-N = 4
-frequency = 1e3
-axis = ax
-
-y = pf.dsp.filter.crossover(impulse, N, frequency)
-pf.plot.freq(y, ax=axis)
-
-axis.set_title('')
-axis.set_xlabel('')
-axis.set_xlim(20, 20e3)
-axis.set_ylim(-95, 5)
-
-plt.tight_layout()
-plt.savefig('filter_types_crossover.png', dpi=150)
-
-# %% audio filter -------------------------------------------------------------
-_, ax = plt.subplots(1, 1, figsize=(15/2.54, 12/2.54))
-
-frequency = 1e3
-axis = ax
-y = pf.dsp.filter.bell(impulse, frequency, 10, 2)
-pf.plot.freq(y, ax=axis, label='Bell')
-
-y = pf.dsp.filter.bell(impulse, frequency, -10, 2)
-pf.plot.freq(y, ax=axis, label='Bell')
-
-y = pf.dsp.filter.high_shelve(impulse, 4*frequency, 10, 2, 'II')
-pf.plot.freq(y * 10**(-20/20), ax=axis, label='High-shelve')
-
-y = pf.dsp.filter.high_shelve(impulse, 4*frequency, -10, 2, 'II')
-pf.plot.freq(y * 10**(-20/20), ax=axis, label='High-shelve')
-
-y = pf.dsp.filter.low_shelve(impulse, 1/4*frequency, 10, 2, 'II')
-pf.plot.freq(y * 10**(-40/20), ax=axis, label='Low-shelve')
-
-y = pf.dsp.filter.low_shelve(impulse, 1/4*frequency, -10, 2, 'II')
-pf.plot.freq(y * 10**(-40/20), ax=axis, label='Low-shelve')
-
-axis.set_title('')
-axis.set_xlabel('')
-axis.set_xlim(20, 20e3)
-axis.set_ylim(-70, 20)
-axis.legend(loc=4, ncol=3)
-
-plt.tight_layout()
-plt.savefig('filter_types_parametric-eq.png', dpi=150)
--- pyfar/_concepts/fft_concepts/normalization.py
+++ pyfar/_concepts/fft_concepts/normalization.py
@@ -1,111 +1,156 @@
 r"""
-Pyfar implements five normalizations [1]_ that can be applied to spectra. The normalizations are implictly used by the :py:class:`~pyfar.Signal` class and are available from :py:func:`~pyfar.dsp.fft.normalization`. This means that, for a Signal object ``signal``, the spectrum data as ``signal.freq`` are calculated depending on the normalization given by ``signal.fft_norm``. **The time signals do not change regardless of the normalization.**
+Pyfar implements five normalizations [1]_ that can be applied to spectra. The
+normalizations are implictly used by the :py:class:`~pyfar.Signal` class and
+are available from :py:func:`~pyfar.dsp.fft.normalization`. This means that,
+for a Signal object ``signal``, ``signal.freq`` is calculated depending on the
+normalization given by ``signal.fft_norm``. **The time signals do not change
+regardless of the normalization.**
 
-Example Signals
----------------
+In order to illustrate the meaning of the normalizations, [1]_ is summarized
+and the consequences are discussed with respect to (arithmetic) operations.
 
-In order to illustrate the meaning of the normalizations in the following section, [1]_ is summarized and the consequences are discussed with respect to (arithmetic) operations.
-Three signals are used for illustration purposes, all with sampling rate of *N = 44100 number of samples* and a *length of 1s*:
-
-#. An impulse response (IR) signal represented by a band pass filter with a magnitude of :math:`1 \text{V}` in the pass band (which, e.g., could be the output of a microphone for a measured loudspeaker transfer function, a room impulse response ...). In terms of signal types, it is considered as an energy signal.
-#. A sine signal with an amplitude of :math:`1 \text{V}`, which represents a discrete tone (power) signal (where, e.g., a snippet was recorded of).
-#. A white noise signal with an rms value of :math:`1/\sqrt{2} \text{V}`, which represents a broadband stochastic (power) signal (where, e.g., a snippet was recorded of).
-#. A FIR filter, represented by an energy preserving octave filter.
-
-Note that the implied unit is :math:`1` for the FIR filter, not :math:`\text{V}`.
-
-|example signals|
-
-Note that a prefix of 10 was used for the dB calculations for the normalizations ``'power'`` and ``'psd'``.
-
-
-Considering all the signals as finite energy signals (which they are in a strict sense due to their finite representation on disk) is in not meaningful here, though their spectra look comparable. This is illustrated by comparing their energies :math:`W`, defined as
-
-.. math::
-        W = \sum_{n=0}^{N-1} s^2(n)
-
-Spectra look similar: but: totaly different energy!
-So considering strictly concept of power and energy, but: acoutstics, meaning.
+Definitions
+-----------
 
 .. list-table::
+   :widths: 25 75
    :header-rows: 1
 
    * - Norm
      - Equation
-     - Explanation
    * - ``'none'``
      - :math:`X(k) = \sum_{n=0}^{N-1} x(n) e^{-i 2 \pi \frac{k n}{N}}``
-     - Definition of the discrete Fourier transform ENERGY PRESERVING, NO CHANGE WITH PADDED ZEROS
    * - ``'unitary'``
-     - :math:`X_{\text{SS}}(k) = \left\{\begin{array}{ll} X(k) & \forall k=0, k=\frac{N}{2}\\ 2 X(k) & \forall 0<k< \frac{N}{2} \end{array}\right.`
-     -
+     - :math:`X_{\text{SS}}(k) = \left\{\begin{array}{ll} X(k) & \forall k=0,
+       k=\frac{N}{2}\\ 2 X(k) & \forall 0<k< \frac{N}{2} \end{array}\right.`
    * - ``'amplitude'``
      - :math:`\overline{X}_{\text{SS}}(k) = \frac{1}{N} X_{\text{SS}}(k)`
-     -
    * - ``'rms'``
-     - :math:`\overline{X}_{RMS}(k) = \left\{\begin{array}{ll} \frac{1}{\sqrt{2}} \overline{X}_{\text{SS}}(k) = \frac{1}{\sqrt{2} N} X_{\text{SS}}(k) & \forall 0<k< \frac{N}{2}\\ \quad \overline{X}_{\text{SS}}(k) = \frac{1}{N} X_{\text{SS}}(k)  & \forall k=0, k=\frac{N}{2} \end{array}\right.`
-     -
+     - :math:`\overline{X}_{RMS}(k) = \left\{\begin{array}{ll}
+       \frac{1}{\sqrt{2}} \overline{X}_{\text{SS}}(k) & \forall 0<k<
+       \frac{N}{2}\\ \quad \overline{X}_{\text{SS}}(k) & \forall k=0,
+       k=\frac{N}{2} \end{array}\right.`
    * - ``'power'``
-     - :math:`\overline{\overline{X}}(k) = \lvert \overline{X}(k) \lvert ^2 = \frac{1}{N^2} \lvert X(k) \lvert ^2`
-     - = RMS in dB
+     - :math:`\overline{\overline{X}}_{\text{SS}}(k) = \lvert
+       \overline{X}_{\text{RMS}}(k) \lvert ^2`
    * - ``'psd'``
-     - :math:`\overline{\overline{\underline{X}}}(k) = \frac{N}{f_s} \overline{\overline{X}}(k) = \frac{N}{f_s} \lvert \overline{X}(k) \lvert ^2 = \frac{1}{f_s N} \lvert  X(k) \lvert ^2`
-     -
-
-Note that, in contrast to _[1], all SignalSignals: single sided spectrum, for normalization not necessarily
-
-
-The ``none`` normalization refers to the definition of the discrete Fourier spectrum
-
-.. math::
-        X(k) = \sum_{n=0}^{N-1} x(n) e^{-i 2 \pi \frac{k n}{N}}
+     - :math:`\overline{\overline{\underline{X}}}_{\text{SS}}(k) =
+       \frac{N}{f_s} \overline{\overline{X}}_{\text{SS}}(k) = \frac{N}{f_s}
+       \lvert \overline{X}_{\text{RMS}}(k) \lvert ^2`
 
 
-unitary
+Note that all pyfar signals are real-valued, leading to single-sided spectra.
+So there are small differences in the definitions compared to the formulas
+written in [1]_.
+
+Explanations
+------------
+
+* ``'none'``:
+        The definition of the discrete Fourier transform as used in
+        pyfar. It represents the signal's energy, i.e., zeropadding does not
+        change spectrum. Accordingly, this norm is to be used for energy
+        signals such as impulse responses. For power signals, the magnitudes
+        depend on the number of samples ("longer recording = more energy"),
+        therefore a different normalization is appropriate.
+* ``'unitary'``:
+        In pyfar, all spectra are single-sided. For power signals,
+        this conversion requires to multiply the outcome of the discrete
+        Fourier transform with a factor of 2 in order to represent power
+        related measures correctly (e.g., the amplitude or RMS-value, see
+        below).
+* ``'amplitude'``:
+        This normalization considers the dependence of the spectral
+        magnitudes on the number of samples, which is appropriate for discrete
+        tones as the resulting magnitudes in the spectrum can be interpreted as
+        amplitudes.
+* ``'rms'``:
+        The crest factor of sine waves of :math:`\sqrt{2}` is considered
+        to represent RMS-values (a power quantity) resulting in a difference of
+        -3 dB compared to ``'amplitude'``.
+* ``'power'``:
+        In a dB representation, it equals the ``'rms'`` normalization
+        describing a power quantity. For stochastic broadband signals, this
+        results in a dependence of the magnitude on the sampling rate as these
+        are defined by a constant power density (see ``'psd'``).
+* ``'psd'``:
+        Using this normalization, signals are represented as
+        *power densities* (e.g. in V²/Hz), leading to a meaningful
+        representation for broadband stochastic signals but not for discrete
+        tones or impulse responses.
+
+.. list-table:: **Overview on appropriate normalizations**
+   :widths: 20 35 45
+   :header-rows: 1
+
+   * - Signal type
+     - Variation
+     - Normalization
+   * - Energy
+     - Impulse responses
+     - ``'none'``
+   * - Power
+     - Discrete tones
+     - ``'amplitude'``, ``'rms'``, ``'power'``
+   * - Power
+     - Broadband stochastic signals
+     - ``'psd'``
 
 
-amplitude spectrum
+Examples
+--------
 
-.. math::
-        \overline{X}(k) = \frac{1}{N} X(k)
+Three signals are used for illustration purposes, all with sampling rate of
+1000 samples and a sampling rate of 10 kHz:
+
+#. An impulse signal (one at :math:`t=0` and zero otherwise) with a constant
+   spectrum. It is an energy signal, so the appropriate normalization is
+   ``'none'``.
+#. A FIR filter. This energy preserving octave filter represents a
+   general impulse response / transfer function with finite energy (e.g., a
+   measured loudspeaker transfer function, a room impulse response, HRTF ...) It
+   is an energy signal, so the appropriate normalization is ``'none'``.
+#. A sine signal with an amplitude of :math:`1 \text{V}`. It represents a discrete tone
+   of which a snippet was recorded. Accordingly, it possess a finite power but
+   infinite energy, so it is a power signal with appriate normalizations
+   ``'amplitude'``, ``'rms'`` or ``'power'``.
+#. A white noise signal with an RMS
+   value of :math:`1/\sqrt{2} \text{V}`. It represents a broadband stochastic
+   signal of which a snippet was recorded of. Accordingly, the appropriate
+   normalization is ``'psd'``.
+
+Note that the implied units differ and a prefix of 10 is used for the dB
+calculations of the normalizations ``'power'`` and ``'psd'``.
+
+|examples|
 
 
-power spectrum
+* The ``'none'`` normalization gives expected results for the impulse and FIR
+  Filter, but leads to a magnitude of number of samples/2 for the sine signal
+  (1000/2, 60-6 dB). As illustrated, other normalizations than ``'none'`` are
+  not meaningful for the IRs.
+* For the sine signal, ``'unitary'`` normalization considers the factor 2 due
+  to the single-side spectrum (+6 dB compared to ``'none'``). The
+  ``'amplitude'`` normalization considers the number of samples, so the
+  amplitude of 1 V (= 0 dB) is represented in the spectrum. Accordingly, the
+  magnitude of the sine is -3 dBV with ``'rms'`` and ``'power'`` normalization.
+* With ``'psd'`` normalization, the sine's magnitude is reduced by a factor of
+  number of samples / sampling rate (1/10, -10 dB). As discussed above, this
+  normalization is only meaningful for the noise, as is represents a spectral
+  density.
 
-.. math::
-        \overline{\overline{X}}(k) = \lvert \overline{X}(k) \lvert ^2 =
-        \frac{1}{N^2} \lvert X(k) \lvert ^2
-
-
-power spectral density (PSD) or power desity spectrum
-
-.. math::
-        \overline{\overline{\underline{X}}}(k) = \frac{N}{f_s}
-        \overline{\overline{X}}(k) = \frac{N}{f_s} \lvert \overline{X}(k)
-        \lvert ^2 = \frac{1}{f_s N} \lvert  X(k) \lvert ^2
-
-
-RMS spectrum
-
-.. math::
-        \overline{X}_{RMS}(k) = \left\{\begin{array}{ll}
-        \frac{1}{\sqrt{2}} \overline{X}_{\text{SS}}(k) \quad \forall 0<k<
-        \frac{N}{2}\\ \overline{X}_{\text{SS}}(k) \quad \forall k=0,
-        k = \frac{N}{2} \end{array}\right.
-
+For further details, especially on the the background for the power
+normalizations, it is referred to [1]_.
 
 References
 ----------
-.. [1]  J. Ahrens, C. Andersson, P. Höstmad, and W. Kropp, “Tutorial on
-        Scaling of the Discrete Fourier Transform and the Implied Physical
-        Units of the Spectra of Time-Discrete Signals,” Vienna, Austria,
-        May 2020, p. e-Brief 600.
+.. [1]  J. Ahrens, C. Andersson, P. Höstmad, and W. Kropp, “Tutorial on Scaling
+        of the Discrete Fourier Transform and the Implied Physical Units of the
+        Spectra of Time-Discrete Signals,” Vienna, Austria, May 2020, p.
+        e-Brief 600.
 
 
-See :py:mod:`~pyfar.dsp.fft` for a complete documentation.
-
-
-.. |example signals| image:: resources/fft_norms_examples.png
+.. |examples| image:: resources/fft_norms_examples.png
    :width: 100%
-   :alt: Examples signals to illustrate the FFT-normalizations
+   :alt: FFT Normalization Examples
 """
