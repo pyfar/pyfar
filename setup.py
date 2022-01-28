6cb9dacf0d96b15a2344f555e0f58477c1d6dc54
sikersten <simon.kersten@rwth-aachen.de>
2022-01-28 13:25:27 +0100

    Bump version: 0.2.3 → 0.3.0

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/pyfar/pyfar',
-    version='0.2.3',
+    version='0.3.0',
     zip_safe=False,
     python_requires='>=3.7'
 )
