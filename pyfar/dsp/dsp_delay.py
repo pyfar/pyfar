b16ad0d3ee1ce6142e888088c0aff29b131f2fec
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2022-03-13 09:22:25 +0100

    [skip ci] finished fractional part of the delay

--- pyfar/dsp/dsp_delay.py
+++ pyfar/dsp/dsp_delay.py
@@ -1,41 +1,75 @@
 import numpy as np
+from scipy.special import iv as bessel_first_mod
 import pyfar as pf
 
 
-def fractional_delay_sinc(signal, delay, order=30, side_lobe_rejection=60,
+def fractional_delay_sinc(signal, delay, order=30, side_lobe_suppression=60,
                           mode="cut"):
 
     # check input -------------------------------------------------------------
     if not isinstance(signal, (pf.Signal)):
         raise ValueError("Input data has to be of type pyfar.Signal")
-    if side_lobe_rejection <= 0:
+    if side_lobe_suppression <= 0:
         raise ValueError("The order must be > 0")
-    if side_lobe_rejection <= 0:
+    if side_lobe_suppression <= 0:
         raise ValueError("The side lobe rejection must be > 0")
+    if mode not in ["cut", "cyclic"]:
+        raise ValueError(f"mode is '{mode}' but must be 'cut' or 'cyclic'")
 
     # separate integer and fractional delay -----------------------------------
     delay_int = np.atleast_1d(delay).astype(int)
     delay_frac = np.atleast_1d(delay - delay_int)
-    # force the fractional delay to be positive
-    delay_int[delay_frac < 0] -= 1
-    delay_frac[delay_frac < 0] += 1
+    # force delay_frac >= 0
+    mask = delay_frac < 0
+    delay_int[mask] -= 1
+    delay_frac[mask] += 1
 
     # get discrete time vector for sinc functions and Kaiser windows ----------
-    # Laakso et al. 1996 Eq (21) applied to the fractional part of the delay
+    # Laakso et al. 1996 Eq. (21) applied to the fractional part of the delay
+    # (Note that int(delay_frac) and round(delay_frac) are always 0 because
+    # np.abs(delay_frac) <= 0.5. We keep them nevertheless for readability and
+    # to make sure M_opt gets the correct dimension)
     if order % 2:
-        M_opt = -(order-1)/2  # int(delay_frac) = 0 in all cases
-                              # because 0 <= delay_frac < 1
+        M_opt = delay_frac.astype("int") - (order-1)/2
     else:
         M_opt = np.round(delay_frac) - order / 2
 
-    # discrete time vector
-    n = np.zeros(delay_frac.shape + (order + 1,))
+    # get matrix versions of the fractional delay and M_opt
+    delay_frac_matrix = np.tile(
+        delay_frac[..., np.newaxis],
+        tuple(np.ones(delay_frac.ndim, dtype="int")) + (order + 1, ))
+    M_opt_matrix = np.tile(
+        M_opt[..., np.newaxis],
+        tuple(np.ones(M_opt.ndim, dtype="int")) + (order + 1, ))
 
-    index = np.nditer(delay_frac, ["multi_index"])
-    for d_frac, m in zip(index, M_opt):
-        n[index.multi_index] = np.arange(m, m + order + 1) - d_frac
+    # discrete time vector
+    n = np.arange(order + 1) + M_opt_matrix - delay_frac_matrix
 
     # get the sinc functions (fractional delay filters) -----------------------
     sinc = np.sinc(n)
 
-    return sinc
+    # get the Kaiser windows --------------------------------------------------
+    # (dsp.time_window can not be used because we need to evaluate the window
+    #  for non integer values)
+
+    # beta parameter for side lobe rejection according to
+    # Oppenheim (2010) Eq. (10.13)
+    beta = pf.dsp.kaiser_window_beta(abs(side_lobe_suppression))
+
+    # Kaiser window according to Oppenheim (2010) Eq. (10.12)
+    alpha = order / 2
+    L = np.arange(order + 1).astype("float") - delay_frac_matrix
+    # required to counter operations on M_opt
+    if order % 2:
+        L += .5
+    else:
+        L[delay_frac_matrix > .5] += 1
+    Z = beta * np.sqrt(np.array(1 - ((L - alpha) / alpha)**2, dtype="complex"))
+    # suppress small imaginary parts
+    kaiser = np.real(bessel_first_mod(0, Z)) / bessel_first_mod(0, beta)
+
+    # apply fractional delay --------------------------------------------------
+    signal = pf.dsp.convolve(
+        signal, pf.Signal(sinc * kaiser, signal.sampling_rate))
+
+    return signal, sinc, kaiser
