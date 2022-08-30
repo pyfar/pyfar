0a57f5e1620169147bc8d6d25976b16c84158218
Anne Heimes <aheimes@posteo.de>
2022-08-30 14:57:48 +0200

    Split read_comsol in read_comsol and read_comsol_header

--- pyfar/io/__init__.py
+++ pyfar/io/__init__.py
@@ -2,7 +2,7 @@ from .io import (read, write,
                  read_sofa, convert_sofa,
                  read_audio, write_audio,
                  audio_subtypes, audio_formats, default_audio_subtype,
-                 read_comsol)
+                 read_comsol, read_comsol_header)
 
 
 __all__ = [
@@ -15,4 +15,5 @@ __all__ = [
     'audio_subtypes',
     'audio_formats',
     'default_audio_subtype',
-    'read_comsol']
+    'read_comsol',
+    'read_comsol_header']
