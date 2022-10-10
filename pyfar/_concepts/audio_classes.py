fb6bd7217e9cdb2b2f50bc19e57c3dbee7ef2676
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2022-10-10 19:51:16 +0200

    follow comments from mberz

--- pyfar/_concepts/audio_classes.py
+++ pyfar/_concepts/audio_classes.py
@@ -25,9 +25,9 @@ conveniently, pyfar defines the channel shape - or ``cshape`` in short - which
 ignores how many samples or frequency bins are stored in an audio object. In
 the above example the cshape would be ``(3, 4)`` and `not` (3, 4, 128).
 
-In analogy, the channel axis - or ``caxis`` in short - refers to an axis of the
-cshape. For example ``caxis = 0`` refers to the first channel axis of
-size 3, and ``caxis = -1`` refers to the last channel axis of size 4 and `not`
+In analogy, the channel axis - or ``caxis`` in short - refers to the dimension
+or axis of the cshape. For example ``caxis = 0`` refers to the first dimension
+of size 3, and ``caxis = -1`` refers to the last dimension of size 4 and `not`
 to the last axis of the data array of size 128. Note that ``caxis`` can also be
 provided as a tuple or list, e.g., ``caxis = (0, 1)`` refers to both channel
 axes of size 3 and 4.
