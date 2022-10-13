2712545b6a4093e80dfc9c96634f136a0ca2507b
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2022-10-13 13:41:52 +0200

    Bump version: 0.4.3 → 0.5.0

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/pyfar/pyfar',
-    version='0.4.3',
+    version='0.5.0',
     zip_safe=False,
     python_requires='>=3.8'
 )
