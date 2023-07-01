caf3fffc85d714440afa177248d896b416608745
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2023-07-01 15:06:51 +0200

    fix concept table

--- pyfar/_concepts/coordinates.py
+++ pyfar/_concepts/coordinates.py
@@ -36,46 +36,56 @@ For more details see the table below.
      - Descriptions
    * - :py:func:`x`, :py:func:`y`, :py:func:`z`
      - x, y, z coordinate of a right handed Cartesian coordinate system in
-       meter (-:math:`\infty` < x,y,z < :math:`\infty`).
+       meter (:math:`-\infty` < x,y,z < :math:`\infty`).
    * - :py:func:`azimuth`
      - Counter clock-wise angle in the x-y plane of the right handed Cartesian
-       coordinate system in radians. 0 radians are defined in positive
-       x-direction, pi/2 radians in positive y-direction and so on
-       (-:math:`\infty` < azimuth < :math:`\infty`, 2 :math:`\pi`-cyclic).
-   * - :py:func:`colatitude`
-     - Angle in the x-z plane of the right handed Cartesian coordinate system
-       in radians. 0 radians colatitude are defined in positive z-direction,
-       pi/2 radians in positive x-direction, and pi in negative z-direction
-       (:math:`\pi`/2 \leq colatitude \leq :math:`\pi`/2). The colatitude is a
-       variation of the elevation angle.
+       coordinate system in radians. :math:`0` radians are defined in positive
+       x-direction, :math:`\pi/2` radians in positive y-direction and so on
+       (:math:`-\infty` < azimuth < :math:`\infty`, :math:`2\pi`-cyclic).
    * - :py:func:`elevation`
      - Angle in the x-z plane of the right handed Cartesian coordinate system
-       in radians. 0 radians elevation are defined in positive x-direction,
-       pi/2 radians in positive z-direction, and -pi/2 in negative z-direction
-       (0 \leq elevation \leq :math:`\pi`). The elevation is a variation of the
-       colatitude.
+       in radians. :math:`0` radians elevation are defined in positive
+       z-direction, :math:`\pi/2` radians in positive x-direction, and
+       :math:`\pi` in negative z-direction
+       (:math:`\pi/2` < elevation < :math:`\pi/2`). The elevation is a
+       variation of the elevation angle.
+   * - :py:func:`colatitude`
+     - Angle in the x-z plane of the right handed Cartesian coordinate system
+       in radians. :math:`0` radians colatitude are defined in positive
+       x-direction, :math:`\pi/2` radians in positive z-direction, and
+       :math:`-\pi/2` in negative z-direction
+       (:math:`0` < colatitude < :math:`\pi`). The colatitude is a
+       variation of the colatitude.
    * - :py:func:`lateral`
      - Counter clock-wise angle in the x-y plane of the right handed Cartesian
-       coordinate system in radians. 0 radians are defined in positive
-       x-direction, pi/2 radians in positive y-direction and -pi/2 in negative
-       y-direction (-:math:`\pi`/2 \leq lateral \leq :math:`\pi`/2).
+       coordinate system in radians. :math:`0` radians are defined in positive
+       x-direction, :math:`\pi/2` radians in positive y-direction and
+       :math:`-\pi/2` in negative y-direction
+       (:math:`-\pi/2` < lateral < :math:`\pi/2`).
+   * - :py:func:`polar`
+     - Angle in the x-z plane of the right handed Cartesian coordinate system
+       in radians. :math:`0` radians elevation are defined in positive
+       x-direction, :math:`\pi/2` radians in positive z-direction,
+       :math:`\pi` in negative x-direction and so on
+       (:math:`-\infty` < polar < :math:`\infty`, :math:`2\pi`-cyclic).
    * - :py:func:`frontal`
      - Angle in the y-z plane of the right handed Cartesian coordinate system
-       in radians. 0 radians elevation are defined in positive y-direction,
-       pi/2 radians in positive z-direction, pi in negative y-direction and
-       so on
-       (-:math:`\infty` < frontal < :math:`\infty`, 2 :math:`\pi`-cyclic).
+       in radians. :math:`0` radians elevation are defined in positive
+       y-direction, :math:`\pi/2` radians in positive z-direction,
+       :math:`\pi` in negative y-direction and so on
+       (:math:`-\infty` < frontal < :math:`\infty`, :math:`2\pi`-cyclic).
    * - :py:func:`upper`
      - Angle in the x-z plane of the right handed Cartesian coordinate system
-       in radians. 0 radians elevation are defined in positive x-direction,
-       pi/2 radians in positive z-direction, and pi in negative x-direction
-       (0 \leq upper \leq :math:`\pi`).
+       in radians. :math:`0` radians elevation are defined in positive
+       x-direction, :math:`\pi/2` radians in positive z-direction, and
+       :math:`\pi` in negative x-direction
+       (:math:`0` < upper < :math:`\pi`).
    * - :py:func:`radius`
      - Distance to the origin of the right handed Cartesian coordinate system
-       in meters (0 \leq radius < :math:`\infty`).
+       in meters (:math:`0` < radius < :math:`\infty`).
    * - :py:func:`rho`
      - Distance perpendicular to the the z-axis of the right handed Cartesian
-       coordinate system (0 \leq rho < :math:`\infty`).
+       coordinate system (:math:`0` < rho < :math:`\infty`).
 
 
 Samplings
