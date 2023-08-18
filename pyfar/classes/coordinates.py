f77f25a91615e4ffd071a66795760d41e1ff0f69
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2023-08-18 12:57:32 +0200

    make plots better

--- pyfar/classes/coordinates.py
+++ pyfar/classes/coordinates.py
@@ -1502,7 +1502,7 @@ class Coordinates():
         .. plot::
 
             >>> import pyfar as pf
-            >>> coords = pf.Coordinates(np.arange(0, 5), 0, 0)
+            >>> coords = pf.Coordinates(np.arange(-5, 5), 0, 0)
             >>> result = coords.find_nearest_cart(2, 0, 0, 0.5, show=True)
 
         """
