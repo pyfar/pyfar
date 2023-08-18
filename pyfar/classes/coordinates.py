958ab496b598f98d72310df3defb9430f814bc1f
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2023-08-18 13:05:42 +0200

    fine tune

--- pyfar/classes/coordinates.py
+++ pyfar/classes/coordinates.py
@@ -1646,7 +1646,7 @@ class Coordinates():
             >>> import pyfar as pf
             >>> import numpy as np
             >>> coords = pf.Coordinates.from_spherical_elevation(
-            >>>     np.arange(-10, 10)*np.pi/180, 0, 1)
+            >>>     np.arange(-30, 30, 5)*np.pi/180, 0, 1)
             >>> result = coords.find_slice('azimuth', 'deg', 0, 5, show=True)
 
         """
