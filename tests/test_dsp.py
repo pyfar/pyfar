ab7873a503fdd73aeb4e2a63cbbebce380cfd777
tluebeck <tim.luebeck@th-koeln.de>
2023-10-15 17:40:45 -0700

    update complex convolution

--- tests/test_dsp.py
+++ tests/test_dsp.py
@@ -765,8 +765,8 @@ def test_convolve_default():
 
 def test_convolve_complex():
     '''Test dsp.convolve with complex signals with default parameters'''
-    x = pf.Signal([1, 0.5, 0.25, 0], 44100, complex=True)
-    y = pf.Signal([1, -1, 0], 44100, complex=True)
+    x = pf.Signal([1, 0.5, 0.25, 0], 44100, is_complex=True)
+    y = pf.Signal([1, -1, 0], 44100, is_complex=True)
 
     res = dsp.convolve(x, y)
     desired = np.array([[1, -0.5, -0.25, -0.25, 0, 0]], dtype='complex')
@@ -808,8 +808,8 @@ def test_convolve_mode_and_method(method, mode, desired):
     ('cyclic', np.array([[0.95, -0.49, 0.1, -0.35]], dtype='complex'))])
 def test_convolve_mode_and_method_complex(method, mode, desired):
     '''Test dsp.convolve with complex signals with various methods and modes'''
-    x = pf.Signal([1, 0.5, 0.5, 0.1], 44100, complex=True)
-    y = pf.Signal([1, -1, 0.1], 44100, complex=True)
+    x = pf.Signal([1, 0.5, 0.5, 0.1], 44100, is_complex=True)
+    y = pf.Signal([1, -1, 0.1], 44100, is_complex=True)
     res = dsp.convolve(x, y, mode=mode, method=method)
     np.testing.assert_allclose(res.time, desired, atol=1e-10)
 
