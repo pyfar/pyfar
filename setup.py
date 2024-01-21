fa54026482200a32584e0fd9f339cf63bd633f67
Anne Heimes <ahe@akustik.rwth-aachen.de>
2024-01-21 15:34:13 +0100

    add test and support for 3.12

--- setup.py
+++ setup.py
@@ -46,7 +46,8 @@ setup(
         'Programming Language :: Python :: 3.8',
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
-        'Programming Language :: Python :: 3.11'
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
     ],
     description="Project for data formats in acoustics.",
     install_requires=requirements,
