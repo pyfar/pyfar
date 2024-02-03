ca054519a85a55ce33b70b769d9f35335fe91bde
Anne Heimes <ahe@akustik.rwth-aachen.de>
2024-02-03 15:47:47 +0100

    separate test copy from deprecations

--- tests/test_copy.py
+++ tests/test_copy.py
@@ -1,8 +1,9 @@
 import pyfar
-
+import pytest
+from pyfar.classes.warnings import PyfarDeprecationWarning
 
 def test_copy(
-        sphericalvoronoi, time_data, frequency_data,
+        time_data, frequency_data,
         filterFIR, filterIIR, filterSOS):
     """ Test copy method used by several classes."""
     obj_list = [pyfar.Signal(1000, 44100),
@@ -12,7 +13,6 @@ def test_copy(
                 filterFIR,
                 filterIIR,
                 filterSOS,
-                sphericalvoronoi,
                 time_data,
                 frequency_data]
 
@@ -27,3 +27,19 @@ def test_copy(
         assert len(obj.__dict__) == len(obj_copy.__dict__)
         # Check if copied objects are equal
         assert obj_copy == obj
+
+
+@pytest.mark.filterwarnings('ignore::Warning')
+def test_copy_sphericalvoronoi(
+        sphericalvoronoi):
+    """ Test copy method used by several classes."""
+    # Create copy
+    obj_copy = sphericalvoronoi.copy()
+    # Check class
+    assert isinstance(obj_copy, sphericalvoronoi.__class__)
+    # Check ID
+    assert id(sphericalvoronoi) != id(obj_copy)
+    # Check attributes
+    assert len(sphericalvoronoi.__dict__) == len(obj_copy.__dict__)
+    # Check if copied objects are equal
+    assert obj_copy == sphericalvoronoi
