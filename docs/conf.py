80dffc377cd5814c86c13dbc530c6bc3024daf12
Marco Berzborn <marco.berzborn@akustik.rwth-aachen.de>
2024-03-15 16:43:05 +0100

    cleanup

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'pyfar'
-copyright = '2020-2024, The pyfar developers'
+copyright = '2020, The pyfar developers'
 author = 'The pyfar developers'
 
 # The version info for the project you're documenting, acts as replacement
@@ -125,74 +125,16 @@ html_theme_options = {
           "icon": "fa-brands fa-square-github",
           "type": "fontawesome",
         },
-        {
-            "name": "CC-BY",
-            "url": "https://creativecommons.org/licenses/by/4.0/deed.de",
-            "icon": "fa-brands fa-creative-commons-by",
-            "type": "fontawesome",
-        }
     ],
-
-  # Configure secondary (right) side bar
-  "show_toc_level": 3,                     # Show all subsections of notebooks
-  "secondary_sidebar_items": ["page-toc"],  # Omit 'show source' link that that
-                                           # shows notebook in json format
-  "navigation_with_keys": True,
+    # Configure secondary (right) side bar
+    "show_toc_level": 3,  # Show all subsections of notebooks
+    "secondary_sidebar_items": ["page-toc"],  # Omit 'show source' link that that shows notebook in json format
+    "navigation_with_keys": True,
 }
 
 html_context = {
    "default_mode": "light"
 }
-# -- Options for Texinfo output ----------------------------------------
-
-# Grouping the document tree into Texinfo files. List of tuples
-# (source start file, target name, title, author,
-#  dir menu entry, description, category)
-# texinfo_documents = [
-#     (master_doc, 'pyfar',
-#      u'pyfar Gallery',
-#      author,
-#      'pyfar',
-#      'One line description of project.',
-#      'Miscellaneous'),
-# ]
-
-# -- Options for nbsphinx -------------------------------------------------
-nbsphinx_prolog = r"""
-{% set docname = 'doc/' + env.doc2path(env.docname, base=None) %}
-
-.. raw:: html
-
-    <div class="admonition note">
-      Open an interactive online version by clicking the badge
-      <span style="white-space: nowrap;"><a href="https://mybinder.org/v2/gh/pyfar/gallery/main?filepath={{ docname|e }}"><img alt="Binder badge" src="https://mybinder.org/badge_logo.svg" style="vertical-align:text-bottom"></a></span>
-      or
-      <a href="{{ env.docname.split('/')|last|e + '.ipynb' }}" class="reference download internal" download>download</a>
-      the notebook.
-      <script>
-        if (document.location.host) {
-          let nbviewer_link = document.createElement('a');
-          nbviewer_link.setAttribute('href',
-            'https://nbviewer.org/url' +
-            (window.location.protocol == 'https:' ? 's/' : '/') +
-            window.location.host +
-            window.location.pathname.slice(0, -4) +
-            'ipynb');
-          nbviewer_link.innerHTML = 'Or view it on <em>nbviewer</em>';
-          nbviewer_link.classList.add('reference');
-          nbviewer_link.classList.add('external');
-          document.currentScript.replaceWith(nbviewer_link, '.');
-        }
-      </script>
-    </div>
-
-"""
-
-# -- manage thumbnails --------------------------------------------------------
-# must be located in 'docs/_static'
-nbsphinx_thumbnails = {
-}
-
 
 # -- pyfar specifics -----------------------------------------------------
 
