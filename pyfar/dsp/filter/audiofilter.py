f778f6f3a74904776922cf86e2c82f168e7d4353
Anton Hoyer <a.hoyer@campus.tu-berlin.de>
2024-04-26 13:48:33 +0200

    simplify code again

--- pyfar/dsp/filter/audiofilter.py
+++ pyfar/dsp/filter/audiofilter.py
@@ -110,13 +110,13 @@ blob/master/filter_design/audiofilter.py
         if coefficients is None:
             coefficients = 0.6436
         # get filter coefficients for first order allpass
-        _, _, b, a = iir.biquad_ap1st(frequency, fs, ai=coefficients)
+        b, a = iir.biquad_ap1st(frequency, fs, ai=coefficients)[2:]
     elif order == 2:
         if coefficients is None:
             coefficients = [0.8832, 1.6278]
         # get filter coefficients for second order allpass
-        _, _, b, a = iir.biquad_ap2nd(
-            frequency, fs, bi=coefficients[0], ai=coefficients[1])
+        b, a = iir.biquad_ap2nd(
+            frequency, fs, bi=coefficients[0], ai=coefficients[1])[2:]
     else:
         raise ValueError('Order must be 1 or 2')
 
