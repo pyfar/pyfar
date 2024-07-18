72a295d3e7fb206e63e44e0fca8a22035a4edd0d
Tim Lübeck <tim.luebeck@icloud.com>
2024-07-18 13:39:24 +0200

    update signal iterator

--- tests/test_audio_signal_iterator.py
+++ tests/test_audio_signal_iterator.py
@@ -17,6 +17,20 @@ def test_simple_iter():
         npt.assert_array_equal(s._data, np.atleast_2d(data[idx, :]))
 
 
+def test_complex_simple_iter():
+    data = np.ones((3, 1024)) * np.atleast_2d(np.arange(3)).T
+    sig = Signal(data, 1)
+    sig.complex = True
+
+    idx = 0
+    for s in sig:
+        npt.assert_array_equal(s._data, np.atleast_2d(data[idx, :]))
+        idx += 1
+
+    for idx, s in enumerate(sig):
+        npt.assert_array_equal(s._data, np.atleast_2d(data[idx, :]))
+
+
 def test_iter_write():
     data = np.ones((3, 1024)) * np.atleast_2d(np.arange(3)).T
     sig = Signal(data, 1)
