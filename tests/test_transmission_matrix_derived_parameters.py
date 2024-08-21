45084f3d4b6256e28d863a1fb4be55d9d22d3ad5
Philipp Schäfer <pschaefer@ITA.AKUSTIK.RWTH-AACHEN.DE>
2024-08-21 15:15:16 +0200

    TransmissionMatrix: adding test for TFs Q1_out / Q2_in and Q2_out / Q1_in

--- tests/test_transmission_matrix_derived_parameters.py
+++ tests/test_transmission_matrix_derived_parameters.py
@@ -135,6 +135,21 @@ def _expected_voltage_tf(twoport_type, Zl : FrequencyData, Z : FrequencyData) ->
         TF.freq[np.isinf(Zl.freq)] = np.nan
     return TF
 
+def _expected_current_to_voltage_tf(twoport_type, Zl : FrequencyData, Z : FrequencyData) -> FrequencyData: #noqa 501
+    if twoport_type not in _twoport_type_list():
+        raise ValueError("Unexpected value for 'twoport_type'")
+
+    if twoport_type == "series_impedance":
+        TF = 1 / (Zl + Z)
+    elif twoport_type == "series_load_impedance":
+        TF = 1 / (2 * Zl)
+        TF.freq[Zl.freq == 0] = np.nan
+    else:
+        TF = 1 / Zl
+        TF.freq[Zl.freq == 0] = np.nan
+
+    return TF
+
 def _expected_current_tf(twoport_type, Zl : FrequencyData, Z : FrequencyData) -> FrequencyData: #noqa 501
     if twoport_type not in _twoport_type_list():
         raise ValueError("Unexpected value for 'twoport_type'")
@@ -153,6 +168,24 @@ def _expected_current_tf(twoport_type, Zl : FrequencyData, Z : FrequencyData) ->
         TF.freq[Zl.freq == 0] = np.nan
     return TF
 
+def _expected_voltage_to_current_tf(twoport_type, Zl : FrequencyData, Z : FrequencyData) -> FrequencyData: #noqa 501
+    if twoport_type not in _twoport_type_list():
+        raise ValueError("Unexpected value for 'twoport_type'")
+
+    idx_inf = np.isinf(Zl.freq)
+    if twoport_type == "parallel_impedance":
+        TF = Z * Zl / (Zl + Z)
+        TF.freq[idx_inf] = Z.freq[idx_inf]
+    elif twoport_type == "parallel_load_impedance":
+        TF = Zl / 2
+        TF.freq[Zl.freq == 0] = np.nan
+        TF.freq[idx_inf] = np.nan
+    else: #bypass, series_impedance + series_load_impedance
+        TF = Zl.copy()
+        TF.freq[idx_inf] = np.nan
+
+    return TF
+
 @pytest.mark.parametrize("impedance_type", ["input", "output"])
 @pytest.mark.parametrize("twoport_type", _twoport_type_list())
 def test_input_impedance(impedance_type : str, twoport_type : str,
@@ -169,22 +202,30 @@ def test_input_impedance(impedance_type : str, twoport_type : str,
     npt.assert_allclose(Zres.freq[idx_default], Zexpected.freq[idx_default], atol=1e-15)
     assert(np.all(np.abs(Zres.freq[idx_inf]) > 1e15))
 
+@pytest.mark.parametrize("tf_type", ["voltage", "current", "voltage/current", "current/voltage"])
 @pytest.mark.parametrize("twoport_type", _twoport_type_list())
-def test_transfer_function_quantity1(twoport_type, load_impedance,impedance_random):
+def test_transfer_function(tf_type, twoport_type, load_impedance,impedance_random):
     """Test for 'voltage' quantity transfer function"""
     tmat = _special_twoport_tmatrix(twoport_type, load_impedance, impedance_random)
-    TF = tmat.transfer_function_quantity1(load_impedance)
-    TF_expected = _expected_voltage_tf(twoport_type, load_impedance, impedance_random)
-    if isinstance(TF_expected, FrequencyData):
-        TF_expected = TF_expected.freq
-    npt.assert_allclose(TF.freq, TF_expected, atol=1e-15)
 
-@pytest.mark.parametrize("twoport_type", _twoport_type_list())
-def test_transferfunction_quantity2(twoport_type, load_impedance,impedance_random):
-    """Test for 'current' quantity transfer function"""
-    tmat = _special_twoport_tmatrix(twoport_type, load_impedance, impedance_random)
-    TF = tmat.transfer_function_quantity2(load_impedance)
-    TF_expected = _expected_current_tf(twoport_type, load_impedance, impedance_random)
+    if tf_type == "voltage":
+        quantity_indices = (0,0)
+        TF_expected = _expected_voltage_tf(twoport_type, load_impedance, impedance_random)
+    elif tf_type == "current":
+        quantity_indices = (1,1)
+        TF_expected = _expected_current_tf(twoport_type, load_impedance, impedance_random)
+    elif tf_type == "voltage/current":
+        quantity_indices = (0,1)
+        TF_expected = _expected_voltage_to_current_tf(twoport_type, load_impedance, impedance_random)
+        # return
+    elif tf_type == "current/voltage":
+        quantity_indices = (1,0)
+        TF_expected = _expected_current_to_voltage_tf(twoport_type, load_impedance, impedance_random)
+        # return
+    else:
+        raise ValueError("Unexpected value for 'tf_type'.")
+
+    TF = tmat.transfer_function(quantity_indices, load_impedance)
     if isinstance(TF_expected, FrequencyData):
         TF_expected = TF_expected.freq
     npt.assert_allclose(TF.freq, TF_expected, atol=1e-15)
