09dd2b7e16852bfe5c7908aeaff36ce6c0109f86
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2024-09-13 14:06:02 +0300

    rerun tests the stupid way

--- pyfar/dsp/filter/_audiofilter.py
+++ pyfar/dsp/filter/_audiofilter.py
@@ -27,7 +27,7 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 
-"""  # noqa: E501
+"""   # noqa: E501
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 import numpy as np
