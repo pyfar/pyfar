05815fc99fda12768b5950050c6f3f01f77f71ef
Anne Heimes <ahe@akustik.rwth-aachen.de>
2024-10-25 11:06:57 +0200

    add python 3.13

--- setup.py
+++ setup.py
@@ -47,7 +47,8 @@ setup(
         'Programming Language :: Python :: 3.9',
         'Programming Language :: Python :: 3.10',
         'Programming Language :: Python :: 3.11',
-        'Programming Language :: Python :: 3.12'
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
     ],
     description="Project for data formats in acoustics.",
     install_requires=requirements,
