715b68e232ca854da058a29a769f069eace5dd38
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2025-01-06 10:54:05 +0100

    Update tests/test_coordinates_find.py
    Co-authored-by: Anton Hoyer <156099087+hoyer-a@users.noreply.github.com>

--- tests/test_coordinates_find.py
+++ tests/test_coordinates_find.py
@@ -208,10 +208,12 @@ def test_find_within_error():
         coords.find_within(find, 1, return_sorted=-1)
 
     find.radius = .1
-    match = 'find_within only works if all points have the same'
+    radius_tol = 0.1
+    match = 'find_within only works if all points have the same radius. '
+            f'Differences are larger than {radius_tol}'
     with pytest.raises(ValueError, match=match):
         coords.find_within(
-            find, distance_measure='spherical_radians', radius_tol=0.1)
+            find, distance_measure='spherical_radians', radius_tol=radius_tol)
 
 
 @pytest.mark.parametrize('distance_measure', [
