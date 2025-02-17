65ffe598a62f78137fca0b55a61c702cb8eabcaf
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2025-02-17 12:41:18 +0100

    commit changes

--- pyfar/constants/__init__.py
+++ pyfar/constants/__init__.py
@@ -3,8 +3,15 @@
 
 from .speed_of_sound import (
     speed_of_sound_simple,
+    speed_of_sound_ideal_gas,
+)
+
+from .utils import (
+    saturation_vapor_pressure,
 )
 
 __all__ = [
     'speed_of_sound_simple',
+    'speed_of_sound_ideal_gas',
+    'saturation_vapor_pressure',
 ]
--- pyfar/constants/speed_of_sound.py
+++ pyfar/constants/speed_of_sound.py
@@ -1,5 +1,6 @@
 """File containing all speed of sound calculation functions."""
 import numpy as np
+from . import utils
 
 
 def speed_of_sound_simple(temperature):
@@ -43,3 +44,92 @@ def speed_of_sound_simple(temperature):
 
     t_0 = 20
     return 343.2*np.sqrt((temperature+273.15)/(t_0+273.15))
+
+
+def speed_of_sound_ideal_gas(
+        temperature, relative_humidity, atmospheric_pressure=101325,
+        saturation_vapor_pressure=None):
+    """Calculate speed of sound in air using the ideal gas law.
+
+    The speed of sound in air can be calculated based on chapter 6.3 in [#]_.
+    All input parameters must be broadcastable to the same shape.
+
+    Parameters
+    ----------
+    temperature : float, array_like
+        Temperature in degree Celsius.
+    relative_humidity : float, array_like
+        Relative humidity in the range of 0 to 1.
+    atmospheric_pressure : float, array_like, optional
+        Atmospheric pressure in pascal, by default 101325 Pa.
+    saturation_vapor_pressure : float, array_like, optional
+        Saturation vapor pressure in Pa.
+        If not given, the function
+        :py:func:`~pyfar.constants.saturation_vapor_pressure` is used.
+        Note that the valid temperature range is therefore also dependent on
+        :py:func:`~pyfar.constants.saturation_vapor_pressure`.
+
+    Returns
+    -------
+    speed_of_sound : float, array_like
+        Speed of sound in air in (m/s).
+
+    References
+    ----------
+    .. [#] V. E. Ostashev and D. K. Wilson, Acoustics in Moving Inhomogeneous
+           Media, 2nd ed. London: CRC Press, 2015. doi: 10.1201/b18922.
+    """
+    # check inputs
+    if not isinstance(temperature, (int, float, np.ndarray, list, tuple)):
+        raise TypeError(
+            'Temperature must be a number or array of numbers')
+    if not isinstance(
+            relative_humidity, (int, float, np.ndarray, list, tuple)):
+        raise TypeError(
+            'Relative humidity must be a number or array of numbers')
+    if not isinstance(
+            atmospheric_pressure, (int, float, np.ndarray, list, tuple)):
+        raise TypeError(
+            'Atmospheric pressure must be a number or array of numbers')
+    temperature = np.array(temperature, dtype=float)
+    temperature_kelvin = temperature + 273.15
+    if np.any(np.array(temperature_kelvin) < 0):
+        raise ValueError("Temperature must be above -273.15°C.")
+    if np.any(np.array(relative_humidity) < 0) or np.any(
+            np.array(relative_humidity) > 1):
+        raise ValueError("Relative humidity must be between 0 and 1.")
+    if np.any(np.array(atmospheric_pressure) < 0):
+        raise ValueError("Atmospheric pressure must be larger than 0 Pa.")
+
+    P = np.array(atmospheric_pressure, dtype=float)  # Pa
+    relative_humidity = np.array(relative_humidity, dtype=float)
+
+    # Constants according to 6.3.2
+    R = 8.314  # J/(K mol)
+    gamma_a = 1.400
+    gamma_w = 1.330
+    mu_a = 28.97*1e-3  # kg/mol
+    mu_w = 18.02*1e-3  # kg/mol
+    R_a = R / mu_a
+
+    # partial pressure of water vapor in Pa
+    if saturation_vapor_pressure is None:
+        p = utils.saturation_vapor_pressure(temperature)  # Pa
+    else:
+        p = np.array(saturation_vapor_pressure, dtype=float)  # Pa
+    e = relative_humidity * p  # Pa
+
+    # next to Equation 6.69
+    alpha = mu_a / mu_w
+    # Equation 6.80
+    delta = (1 - (1/gamma_a)) / (1 - (1/gamma_w))
+    nu = (gamma_a - 1) / (gamma_w - 1)
+
+    # Equation 6.70
+    C = (e/P) / (alpha * (1 - e/P))
+
+    # Equation 6.84
+    return np.sqrt(
+        gamma_a * R_a * temperature_kelvin * (
+            1 + (alpha * (1 + delta - nu) - 1) * C),
+    )
