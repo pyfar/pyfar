209dd393090bbe7bbc597be0fb42aaa90e105be3
Anne Heimes <64446926+ahms5@users.noreply.github.com>
2025-02-17 12:30:31 +0100

    cleanup pr

--- pyfar/constants/__init__.py
+++ pyfar/constants/__init__.py
@@ -3,20 +3,13 @@
 
 from .speed_of_sound import (
     speed_of_sound_simple,
-    speed_of_sound_ideal_gas,
 )
 
 from .medium_attenuation import (
     air_attenuation,
 )
 
-from .utils import (
-    saturation_vapor_pressure,
-)
-
 __all__ = [
     'speed_of_sound_simple',
-    'speed_of_sound_ideal_gas',
     'air_attenuation',
-    'saturation_vapor_pressure',
 ]
