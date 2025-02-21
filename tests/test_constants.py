eb30a5c0c0aad10cbd78ff893aa745fcd392a1f8
Anne Heimes <ahe@akustik.rwth-aachen.de>
2025-02-21 16:39:48 +0100

    add test

--- tests/test_constants.py
+++ tests/test_constants.py
@@ -5,3 +5,6 @@ def test_standard_atmosphere_pressure():
 
 def test_absolute_zero_celsius():
     assert pf.constants.absolute_zero_celsius == -273.15
+
+def test_standard_air_density():
+    assert pf.constants.standard_air_density == 1.204
