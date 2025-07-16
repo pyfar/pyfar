7679c6d78911aa482533b09985014e71a0e47122
Johannes Sommerfeldt <j.sommerfeldt@tu-berlin.de>
2025-07-16 10:26:49 +0200

    changed frequency weighting tests to use pytest parametrization intsead of loops

--- tests/test_frequency_weighting.py
+++ tests/test_frequency_weighting.py
@@ -2,26 +2,14 @@ import numpy as np
 from numpy import testing as npt
 import pyfar
 import pyfar.dsp.filter as pffilt
+import pytest
 
 
-def test_frequency_weighting_constants():
-    weightings = ["A", "C"]
-    band_options = ["octave", "third"]
-    freq_ranges = [
-        (20, 20000),
-        (0, 100),
-        (4000, 100_000),
-    ]
-    for freq_range in freq_ranges:
-        for weighting in weightings:
-            for bands in band_options:
-                _test_frequency_weighting_constants_combination(
-                    weighting, bands, freq_range)
-
-
-def _test_frequency_weighting_constants_combination(
-        weighting, bands, freq_range):
-
+@pytest.mark.parametrize("weighting", ["A", "C"])
+@pytest.mark.parametrize("bands", ["octave", "third"])
+@pytest.mark.parametrize("freq_range", [(20, 20000), (0, 100),
+                                        (4000, 100_000)])
+def test_frequency_weighting_constants(weighting, bands, freq_range):
     (nominals, iec_weights) = pyfar.constants. \
             frequency_weighting_band_corrections(weighting, bands, freq_range)
     calculated_weights = pyfar.constants. \
@@ -33,22 +21,13 @@ def _test_frequency_weighting_constants_combination(
     npt.assert_allclose(calculated_weights, iec_weights, atol=0.3)
 
 
-def test_frequency_weighting_filter():
-    base_rates = np.array([48000, 44100, 16000])
-    test_rates = np.array([
-        base_rates,
-        base_rates / 2, base_rates * 2,
-        base_rates / 4, base_rates * 4,
-        base_rates / 8, base_rates * 8,
-    ]).flatten().tolist()
+@pytest.mark.parametrize("weighting", ["A", "C"])
+@pytest.mark.parametrize("base_rate", [48000, 44100, 16000])
+@pytest.mark.parametrize("rate_factor", [1, 2, 1/2, 4, 1/4, 8, 1/8])
+def test_frequency_weighting_filter(weighting, base_rate, rate_factor):
+    fs = base_rate * rate_factor
+    filt = pffilt.frequency_weighting_filter(None, weighting, sampling_rate=fs)
+    is_class_1, _, _ = pffilt.frequency_weighting. \
+        _check_filter(filt.sampling_rate, filt.coefficients[0], weighting)
+    assert is_class_1
 
-    for weighting in ["A", "C"]:
-        for fs in test_rates:
-            filt = pffilt.frequency_weighting_filter(None,
-                                                     weighting,
-                                                     sampling_rate=fs)
-            is_class_1, _, _ = pffilt.frequency_weighting. \
-                _check_filter(filt.sampling_rate,
-                              filt.coefficients[0],
-                              weighting)
-            assert is_class_1
