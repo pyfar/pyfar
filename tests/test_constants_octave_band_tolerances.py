6f4c2f6647115b4dfa91045208fcf13e886a9654
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2025-08-08 16:27:28 +0200

    update parameter name

--- tests/test_constants_octave_band_tolerances.py
+++ tests/test_constants_octave_band_tolerances.py
@@ -6,23 +6,24 @@ import pytest
 
 
 @pytest.mark.parametrize('exact_center_frequency', [1000, 1000 * 10**.3])
-@pytest.mark.parametrize('bands', ['octave', 'third'])
+@pytest.mark.parametrize('num_fractions', [1, 3])
 @pytest.mark.parametrize('tolerance_class', [1, 2])
 def test_octave_band_tolerance(
-        exact_center_frequency, bands, tolerance_class):
+        exact_center_frequency, num_fractions, tolerance_class):
     """Test values returned by octave_band_tolerance."""
 
     # load reference data
     filename = Path((
         "tests/references/octave_band_tolerance_"
-        f"{int(exact_center_frequency)}Hz_{bands}_class{tolerance_class}.csv"))
+        f"{int(exact_center_frequency)}Hz_num_fractions{num_fractions}_"
+        f"class{tolerance_class}.csv"))
     data = np.loadtxt(filename, delimiter=',')
     frequencies_ref = data[0]
     tolerance_ref = data[1:]
 
     # generate tolerance using pyfar
     tolerance, frequencies = pf.constants.octave_band_tolerance(
-                exact_center_frequency, bands, tolerance_class)
+                exact_center_frequency, num_fractions, tolerance_class)
 
     # actual tests (data written and tested with 2 decimals of precision)
     assert type(tolerance) == np.ndarray
@@ -37,10 +38,10 @@ def test_octave_band_tolerance_errors():
     """Test if all errors are raised as expected."""
 
     with pytest.raises(ValueError, match='The exact center frequency'):
-        pf.constants.octave_band_tolerance([1000], 'octave', 1)
+        pf.constants.octave_band_tolerance([1000], 1, 1)
 
-    with pytest.raises(ValueError, match="bands is 'not working'"):
-        pf.constants.octave_band_tolerance(1000, 'not working', 1)
+    with pytest.raises(ValueError, match="num_fractions is 2"):
+        pf.constants.octave_band_tolerance(1000, 2, 1)
 
     with pytest.raises(ValueError, match="tolerance_class is 0"):
-        pf.constants.octave_band_tolerance(1000, 'octave', 0)
+        pf.constants.octave_band_tolerance(1000, 1, 0)
