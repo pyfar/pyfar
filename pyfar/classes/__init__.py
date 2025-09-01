e11bc3dafb455123096d6c40bda1bc94c2606d24
Hanna <hanna.chmeruk@rwth-aachen.de>
2025-09-01 11:25:34 +0200

    First draft of the _NumPyLike class

--- pyfar/classes/__init__.py
+++ pyfar/classes/__init__.py
@@ -5,7 +5,7 @@ from . import orientations
 from . import filter
 from . import transmission_matrix
 from . import warnings
-from . import pyfar_base
+from . import numpy_like
 
 __all__ = [
     'audio',
@@ -14,6 +14,6 @@ __all__ = [
     'filter',
     'transmission_matrix',
     'warnings',
-    'pyfar_base',
+    'numpy_like',
 
 ]
