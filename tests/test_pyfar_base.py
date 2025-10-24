b61daefcb71aba51d134a35237b39eb14d795b43
Hanna <hanna.chmeruk@rwth-aachen.de>
2025-10-24 08:57:25 +0200

    Replaced the Dummy class by patching __abstractmethods__, renamed files

--- tests/test_pyfar_base.py
+++ tests/test_pyfar_base.py
@@ -1,23 +1,23 @@
-from pyfar.classes._PyfarBase import _PyfarBase
+from pyfar.classes._pyfar_base import _PyfarBase
+from unittest.mock import patch
+import pytest
 
-class Dummy(_PyfarBase):
-    """
-    Minimal dummy class for testing _PyfarBase.
-    """
+@pytest.fixture(autouse=True)
+def _disable_abstract_methods():
+    """Automatically disable abstract methods for tests."""
 
-    def _decode(self):
-        pass
+    with patch.multiple(_PyfarBase,__abstractmethods__=set()):
+            yield
 
 def test_comment():
     """Test the comment property."""
-
-    instance = Dummy("Dummy #1")
+    instance =_PyfarBase("Dummy #1")
     assert instance.comment == "Dummy #1"
 
 def test_comment_setter():
     """Test the comment.setter."""
 
-    instance = Dummy()
+    instance = _PyfarBase()
     assert instance.comment == ''
     instance.comment = "Dummy #2"
     assert instance.comment == "Dummy #2"
@@ -25,22 +25,22 @@ def test_comment_setter():
 def test_copy():
     """Test the copy() function."""
 
-    instance = Dummy("Dummy #3")
+    instance = _PyfarBase("Dummy #3")
     instance_copy = instance.copy()
     assert instance == instance_copy
 
 def test_encode():
     """Test the _encode() function."""
 
-    instance = Dummy("Dummy #4")
+    instance = _PyfarBase("Dummy #4")
     instance_encoded = instance._encode()
     assert instance_encoded == {'_comment': 'Dummy #4'}
 
 def test_eq():
     """Test the __eq__() function."""
 
-    instance_1 = Dummy("Dummy #5")
-    instance_2 = Dummy("Dummy #5")
-    instance_3 = Dummy("Dummy #6")
+    instance_1 = _PyfarBase("Dummy #5")
+    instance_2 = _PyfarBase("Dummy #5")
+    instance_3 = _PyfarBase("Dummy #6")
     assert instance_1 == instance_2
     assert instance_1 != instance_3
