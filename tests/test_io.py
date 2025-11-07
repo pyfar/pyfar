3433d46f1a82955c5c6c1d7b16c2cfcf7a0a7182
Fabian Brinkmann <fabian.brinkmann@mailbox.org>
2025-11-07 15:52:46 +0100

    fix warning due to deprecation in io

--- tests/test_io.py
+++ tests/test_io.py
@@ -705,7 +705,7 @@ def test_audio_subtypes(audio_subtypes_mock):
 def test_default_audio_subtype(default_audio_subtype_mock):
     """Test correct call of the wrapped soundfile function."""
     audio_format = 'wav'
-    subtype_return = pyfar.io.default_audio_subtype(format=audio_format)
+    subtype_return = pyfar.io.default_audio_subtype(audio_format=audio_format)
     assert subtype_return == 'bla'
     default_audio_subtype_mock.assert_called_with(audio_format)
 
