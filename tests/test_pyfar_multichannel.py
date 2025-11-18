ee2cfa7e51aee581f86fab0d62451e0f47dc4a41
Hanna <hanna.chmeruk@rwth-aachen.de>
2025-11-18 15:32:46 +0100

    Added blank lines between tests in the test_pyfar_multichannel.py file

--- tests/test_pyfar_multichannel.py
+++ tests/test_pyfar_multichannel.py
@@ -9,12 +9,14 @@ def _disable_abstract_methods():
     with patch.multiple(_PyfarMultichannel,__abstractmethods__=set()):
         yield
 
+
 def test_cshape():
     """Test the cshape property."""
     data = np.array([[[1, 1, 1], [1, 1, 1]], [[2, 2, 2], [2, 2, 2]]])
     instance = _PyfarMultichannel()
     instance._data = data
     assert instance.cshape == (2,2)
+
 
 def test_cdim():
     """Test the cdim property."""
@@ -23,12 +25,14 @@ def test_cdim():
     instance._data = data
     assert instance.cdim == (1)
 
+
 def test_csize():
     """Test the csize property."""
     data = np.array([[[1, 1, 1], [1, 1, 1]], [[2, 2, 2], [2, 2, 2]]])
     instance = _PyfarMultichannel()
     instance._data = data
     assert instance.csize == 4
+
 
 @pytest.mark.parametrize('cshape', [(4, 1), (1, 4)])
 def test_reshape(cshape):
@@ -39,6 +43,7 @@ def test_reshape(cshape):
     reshaped = instance.reshape(cshape)
     assert reshaped.cshape == cshape
 
+
 @pytest.mark.parametrize('cshape', [(5, 1), (2, 3)])
 def test_reshape_value_error(cshape):
     """Test whether ValueError is raised for the reshape method."""
@@ -48,6 +53,7 @@ def test_reshape_value_error(cshape):
     with pytest.raises(ValueError, match='reshape an object of cshape'):
         instance.reshape(cshape)
 
+
 def test_flatten():
     """Test the flatten method."""
     data = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
@@ -55,6 +61,7 @@ def test_flatten():
     instance._data = data
     flatted = instance.flatten()
     assert flatted.cshape == (4,)
+
 
 @pytest.mark.parametrize('cshape', [(3, 3), (6, 3), (7, 5, 3)])
 def test_broadcast_cshape(cshape):
@@ -65,14 +72,16 @@ def test_broadcast_cshape(cshape):
     broadcasted = instance.broadcast_cshape(cshape)
     assert broadcasted.cshape == cshape
 
+
 @pytest.mark.parametrize('cdim', [1, 2, 5, 9])
 def test_broadcast_cdim(cdim):
-    """Test the broadcast_csize method."""
+    """Test the broadcast_cdim method."""
     data = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
     instance = _PyfarMultichannel()
     instance._data = data
     broadcasted = instance.broadcast_cdim(cdim)
     assert broadcasted.cdim == cdim
+
 
 def test_broadcast_cdim_value_error():
     """Test whether ValueError is raised for the broadcast_csize method."""
