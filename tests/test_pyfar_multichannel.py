33bd1d29a51a0e44f2d9b14bb93d565cd5ae279c
Hanna <hanna.chmeruk@rwth-aachen.de>
2026-01-15 14:50:29 +0100

    Added implementation for the transpose() function, added test functions

--- tests/test_pyfar_multichannel.py
+++ tests/test_pyfar_multichannel.py
@@ -2,14 +2,9 @@ from pyfar.classes._pyfar_multichannel import _PyfarMultichannel
 import numpy as np
 import pytest
 from unittest.mock import patch
+import numpy.testing as npt
 
-@pytest.fixture(autouse=True)
-def _disable_abstract_methods():
-    """Automatically disable abstract methods."""
-    with patch.multiple(_PyfarMultichannel,__abstractmethods__=set()):
-        yield
-
-
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
 def test_cshape():
     """Test the cshape property."""
     data = np.array([[[1, 1, 1], [1, 1, 1]], [[2, 2, 2], [2, 2, 2]]])
@@ -18,6 +13,7 @@ def test_cshape():
     assert instance.cshape == (2,2)
 
 
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
 def test_cdim():
     """Test the cdim property."""
     data = np.array([[1, 2, 3], [4, 5, 6]])
@@ -26,6 +22,7 @@ def test_cdim():
     assert instance.cdim == (1)
 
 
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
 def test_csize():
     """Test the csize property."""
     data = np.array([[[1, 1, 1], [1, 1, 1]], [[2, 2, 2], [2, 2, 2]]])
@@ -34,6 +31,7 @@ def test_csize():
     assert instance.csize == 4
 
 
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
 @pytest.mark.parametrize('cshape', [(4, 1), (1, 4)])
 def test_reshape(cshape):
     """Test the reshape method."""
@@ -44,6 +42,7 @@ def test_reshape(cshape):
     assert reshaped.cshape == cshape
 
 
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
 @pytest.mark.parametrize('cshape', [(5, 1), (2, 3)])
 def test_reshape_value_error(cshape):
     """Test whether ValueError is raised for the reshape method."""
@@ -54,6 +53,7 @@ def test_reshape_value_error(cshape):
         instance.reshape(cshape)
 
 
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
 def test_flatten():
     """Test the flatten method."""
     data = np.array([[[1, 2, 3], [4, 5, 6]], [[7, 8, 9], [10, 11, 12]]])
@@ -63,6 +63,7 @@ def test_flatten():
     assert flatted.cshape == (4,)
 
 
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
 @pytest.mark.parametrize('cshape', [(3, 3), (6, 3), (7, 5, 3)])
 def test_broadcast_cshape(cshape):
     """Test the broadcast_cshape method."""
@@ -73,6 +74,7 @@ def test_broadcast_cshape(cshape):
     assert broadcasted.cshape == cshape
 
 
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
 @pytest.mark.parametrize('cdim', [1, 2, 5, 9])
 def test_broadcast_cdim(cdim):
     """Test the broadcast_cdim method."""
@@ -83,6 +85,7 @@ def test_broadcast_cdim(cdim):
     assert broadcasted.cdim == cdim
 
 
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
 def test_broadcast_cdim_value_error():
     """Test whether ValueError is raised for the broadcast_csize method."""
     data = np.array([[[[1, 2, 3]]]])
@@ -90,3 +93,39 @@ def test_broadcast_cdim_value_error():
     instance._data = data
     with pytest.raises(ValueError, match='channel dimensions exceeds'):
         instance.broadcast_cdim(2)
+
+
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
+def test_transpose():
+    rng = np.random.default_rng()
+    data = rng.random((6, 2, 5, 256))
+    instance1 = _PyfarMultichannel()
+    instance1._data = data
+    instance2 = instance1.transpose()
+    npt.assert_allclose(instance1.T._data, instance2._data)
+    npt.assert_allclose(instance1._data.transpose(2, 1, 0, 3), instance2._data)
+
+
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
+@pytest.mark.parametrize('taxis', [(2, 0, 1), (-1, 0, -2)])
+def test_transpose_positional_arguments(taxis):
+    rng = np.random.default_rng()
+    data = rng.random((6, 2, 5, 256))
+    instance1 = _PyfarMultichannel()
+    instance1._data = data
+    instance2 = instance1.transpose(taxis)
+    npt.assert_allclose(instance1._data.transpose(2, 0, 1, 3), instance2._data)
+    instance2 = instance1.transpose(*taxis)
+    npt.assert_allclose(instance1._data.transpose(2, 0, 1, 3), instance2._data)
+
+
+@patch.multiple(_PyfarMultichannel, __abstractmethods__=set())
+def test_transpose_errors():
+    rng = np.random.default_rng()
+    data = rng.random((10, 10, 256))
+    instance = _PyfarMultichannel()
+    instance._data = data
+    with pytest.raises(ValueError, match="Number of axes must match the cdim"):
+        instance.transpose(1, 0, 2, 3)
+    with pytest.raises(ValueError, match="Axes must be a rearrangement of cdim"):
+        instance.transpose(2, 1)
