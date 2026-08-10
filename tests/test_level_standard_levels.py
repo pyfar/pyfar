ac00337af22ba0f0a7f99e8042e14b0f806d2095
Johannes Sommerfeldt <j.sommerfeldt@tu-berlin.de>
2026-08-10 09:46:31 +0200

    changed hard-coded constants to defined values for sliding Leq tests

--- tests/test_level_standard_levels.py
+++ tests/test_level_standard_levels.py
@@ -27,10 +27,9 @@ def test_level_sliding_equivalent_continuous_level_known_value():
     s = pf.signals.sine(1000, 44100, phase=0.01, sampling_rate=44100)
     levels = pf.level.sliding_equivalent_continuous_level(
         s, "Z", None, 1, False, False, 2e-5)
-    # 94 dB is 1 Pa, -3.01 dB is the crest factor of sine signals
     # with 1 second window size, the value at 1 second should be
     # the same as the equivalent continuous level of the full signal
-    assert np.isclose(levels[0][-1], 94 - 3.01, atol=0.1)
+    assert np.isclose(levels[0][-1], ONE_PA - SINE_PAPR, atol=0.001)
 
 
 def test_level_sliding_equivalent_continuous_level_shape():
